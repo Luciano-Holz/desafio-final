360a80d4e32604f4066ee036d47bd11fa09c9dc5
Luciano Holz <luciano.holz@hotmail.com>
2021-10-20 19:14:57 -0300

    Desestructure values

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -1,8 +1,11 @@
 //file with class Carcontroller
+const CarService = require('../service/CarService');
 
 class CarController {
     async create(req, res) {
-        const result = await CarService.create(req.body);
+        const results = await CarService.create(req.body);
+        const  { modelo, cor, ano, acessorios, quantidadePassageiros } = results;
+        const result = { modelo, cor, ano, acessorios, quantidadePassageiros };
         return res.status(201).json(result);
     }
 }
