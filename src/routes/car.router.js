806051a1dc6b9c3f70456e3a507e83bbb1668f31
Luciano Holz <luciano.holz@hotmail.com>
2021-10-20 14:42:57 -0300

    Add const createValidation on file car.router.js

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,8 +1,9 @@
 //file by routes car
-
+const CarController = require('../app/controller/CarController');
+const createValidation = require('../app/validation/car/create');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-    routes.post('/', CarController.create);
+    routes.post('/',createValidation , CarController.create);
   
     server.use(prefix, routes);
 }
