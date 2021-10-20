2ce3478affc12e8237cfbc04fd265a1e77ccb04b
Luciano Holz <luciano.holz@hotmail.com>
2021-10-20 15:14:27 -0300

    Create folder repository and file CarRepository.js

---
+++ src/app/repository/CarRepository.js
@@ -0,0 +1,10 @@
+//file repositorys
+const CarSchema = require('../schema/CarSchema');
+
+class CarRepository {
+    async create(payload) {
+        return CarSchema.create(payload);
+    }
+}
+
+module.exports = new CarRepository();
