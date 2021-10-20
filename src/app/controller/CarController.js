2125d652be2d39100d7c0e24699af2321f820202
Luciano Holz <luciano.holz@hotmail.com>
2021-10-20 11:06:30 -0300

    Create folder controller and file CarController.js

---
+++ src/app/controller/CarController.js
@@ -0,0 +1,10 @@
+//file with class Carcontroller
+
+class CarController {
+    async create(req, res) {
+        const result = await CarService.create(req.body);
+        return res.status(201).json(result);
+    }
+}
+
+module.exports = new CarController();
