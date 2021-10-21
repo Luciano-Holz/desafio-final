59ffc61989a927db248be59089750c622f182382
Luciano Holz <luciano.holz@hotmail.com>
2021-10-21 14:48:31 -0300

    Adding find method

--- src/app/repository/CarRepository.js
+++ src/app/repository/CarRepository.js
@@ -5,6 +5,12 @@ class CarRepository {
     async create(payload) {
         return CarSchema.create(payload);
     }
+    async find() {
+        return CarSchema.find();
+    }
+    // async findOne(_id) {
+    //     return await CarSchema.findOne({_id}).exec();
+    // }
 }
 
 module.exports = new CarRepository();
