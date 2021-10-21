5fbae6bf41afdfaf183a9ecf80e2a3a140191238
Luciano Holz <luciano.holz@hotmail.com>
2021-10-21 14:46:35 -0300

    Adding get route

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -4,6 +4,8 @@ const createValidation = require('../app/validation/car/create');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/',createValidation , CarController.create);
+    routes.get('/', CarController.find);
+    //routes.get('/:_id', CarController.findOne);
   
     server.use(prefix, routes);
 }
