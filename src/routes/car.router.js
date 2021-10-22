284f553518bca02b63a6a80c85c88d90eaa11ac7
Luciano Holz <luciano.holz@hotmail.com>
2021-10-22 14:31:31 -0300

    Adding route put

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,9 +3,10 @@ const createValidation = require('../app/validation/car/create');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/',createValidation , CarController.create);
-    routes.get('/', CarController.find);
-    routes.get('/:_id', CarController.findOne);
-    routes.delete('/:_id', CarController.deleteOne);
+    routes.get('/', CarController.getAll);
+    routes.get('/:_id', CarController.getById); 
+    routes.put('/:_id',createValidation , CarController.update); 
+    routes.delete('/:_id', CarController.delete); 
   
     server.use(prefix, routes);
 }
