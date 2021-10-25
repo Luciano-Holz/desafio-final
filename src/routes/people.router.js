4b406411d76ac94a5915cba2af087c8fe2adb982
Luciano Holz <luciano.holz@hotmail.com>
2021-10-25 09:57:36 -0300

    Add delete route

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -6,6 +6,7 @@ module.exports = (server, routes, prefix = '/api/v1/people') => {
     routes.get('/', PeopleController.getAll);
     routes.get('/:_id', PeopleController.getById);
     routes.put('/:_id', createValidation, PeopleController.update);
+    routes.delete('/:_id', PeopleController.delete);
 
     server.use(prefix, routes);
 }
