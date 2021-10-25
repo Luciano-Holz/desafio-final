90d0cc4234208b8d297562b6b90c290de7bfb6a8
Luciano Holz <luciano.holz@hotmail.com>
2021-10-25 08:51:32 -0300

    Change validators on field cpf

--- src/app/validation/people/create.js
+++ src/app/validation/people/create.js
@@ -1,17 +1,14 @@
-const { validator } = require('cpf-cnpj-validator');
-const Joi = require('@hapi/joi').extend(validator);
-const cpfSchema = Joi.document().cpf();
 
+const Joi = require('joi');
 
 module.exports = async (req, res, next) => {
     try {
         const schema = Joi.object({
             nome: Joi.string()
                 .required(),
-            cpf: Joi.number()
-                .validate()
-                .required()
-                .integer(),
+            cpf: Joi.string()
+                .pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/))
+                .required(),
             data_nascimento: Joi.date()
                 .required(),
             email: Joi.string()
@@ -23,7 +20,6 @@ module.exports = async (req, res, next) => {
                 .required()
         });
         
-
         const { error } = await schema.validate(req.body, { abortEarly: true });
         if(error) throw  error 
         return next();
