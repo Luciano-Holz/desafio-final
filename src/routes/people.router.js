e3a163fb0441c7d136f65723c865cf7dba33cf88
Luciano Holz <luciano.holz@hotmail.com>
2021-10-25 09:44:04 -0300

    Add update route

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -5,6 +5,7 @@ module.exports = (server, routes, prefix = '/api/v1/people') => {
     routes.post('/', createValidation, PeopleController.create);
     routes.get('/', PeopleController.getAll);
     routes.get('/:_id', PeopleController.getById);
+    routes.put('/:_id', createValidation, PeopleController.update);
 
     server.use(prefix, routes);
 }
