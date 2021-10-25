49612e273a47db7906097c0fb42df1013d674774
Luciano Holz <luciano.holz@hotmail.com>
2021-10-25 08:40:42 -0300

    Add get route

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -3,7 +3,7 @@ const createValidation = require('../app/validation/people/create');
 
 module.exports = (server, routes, prefix = '/api/v1/people') => {
     routes.post('/', createValidation, PeopleController.create);
-
+    routes.get('/', createValidation, PeopleController.getAll);
 
     server.use(prefix, routes);
 }
