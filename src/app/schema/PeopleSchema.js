2d55c730b4bf1e14a4755a993b12efbb4c2add36
Luciano Holz <luciano.holz@hotmail.com>
2021-10-25 08:50:14 -0300

    Change value field cpf

--- src/app/schema/PeopleSchema.js
+++ src/app/schema/PeopleSchema.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const PeopleSchema = mongoose.Schema({
     nome: String,
-    cpf: Number,
+    cpf: String,
     data_nascimento: Date,
     email: String,
     senha: String,
     habilitado: {
         type: String,
         enum: ['sim', 'nao']
-    }
+    } 
 })
 
 const People = mongoose.model('People', PeopleSchema);
