cfa33c5e3e4dafafdd52ea566b1ff2665d06e49e
Luciano Holz <luciano.holz@hotmail.com>
2021-10-26 15:31:45 -0300

    Change fields from schema People

--- src/app/schema/PeopleSchema.js
+++ src/app/schema/PeopleSchema.js
@@ -1,15 +1,44 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const PeopleSchema = mongoose.Schema({
-    nome: String,
-    cpf: String,
-    data_nascimento: Date,
-    email: String,
-    senha: String,
+    nome: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    },
+    data_nascimento: {
+        type: Date,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    senha: {
+        type: String,
+        required: true,
+        select: false,
+        set: value => 
+            crypto
+            .createHash('md5')
+            .update(value)
+            .digest('hex')
+    },
     habilitado: {
         type: String,
+        required: true,
         enum: ['sim', 'nao']
-    } 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 const People = mongoose.model('People', PeopleSchema);
