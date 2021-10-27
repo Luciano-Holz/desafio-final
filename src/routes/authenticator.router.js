dd3e970ac9b8d05e77e07ad021c9146a8a151772
Luciano Holz <luciano.holz@hotmail.com>
2021-10-27 15:58:11 -0300

    Change path const UserController

--- src/routes/authenticator.router.js
+++ src/routes/authenticator.router.js
@@ -1,4 +1,4 @@
-const UserController = require('../app/controller/UserController');
+const UserController = require('../app/controller/AuthenticatorController');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
     routes.post('/', UserController.auth);
