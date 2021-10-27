7017634df4a70d632374447d6bdf4f7ac38b9a0a
Luciano Holz <luciano.holz@hotmail.com>
2021-10-27 08:38:22 -0300

    Create file AuthenticatorController.js and add auth method

---
+++ src/app/controller/AuthenticatorController.js
@@ -0,0 +1,31 @@
+const UserService = require('../service/AuthenticatorService');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
+
+
+class UserController {
+    async auth(req, res) {
+        
+        try {
+            const result = await UserService.auth(req.body);
+            if(!result) {
+                return res.status(404).json('Not found authentication')
+            }
+            
+            const { _id, nome, cpf, data_nascimento, senha, createdAt, __v, ...authentication } = result.toObject();
+            function generateToken(params = {}) {
+                return jwt.sign(params, authConfig.secret, { expiresIn: 86400});
+            }
+            
+            return res.status(201)
+                .json({authentication, token: generateToken({id: authentication._id})});
+
+        } catch (error) {
+            return error;
+        }
+       
+    }
+}
+
+module.exports = new UserController();
