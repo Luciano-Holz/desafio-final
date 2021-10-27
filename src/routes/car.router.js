f56003ab9f6939aac54cbfc1906b1e69e557a54c
Luciano Holz <luciano.holz@hotmail.com>
2021-10-27 08:42:19 -0300

    Add const authMiddleware on routes getId, put and delete

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,12 +1,13 @@
 const CarController = require('../app/controller/CarController');
 const createValidation = require('../app/validation/car/create');
+const authMiddleware = require('../app/middlewares/auth');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/',createValidation , CarController.create);
     routes.get('/', CarController.getAll);
-    routes.get('/:_id', CarController.getById); 
-    routes.put('/:_id',createValidation , CarController.update); 
-    routes.delete('/:_id', CarController.delete); 
+    routes.get('/:_id',authMiddleware, CarController.getById); 
+    routes.put('/:_id', authMiddleware, createValidation , CarController.update); 
+    routes.delete('/:_id',authMiddleware, CarController.delete); 
   
     server.use(prefix, routes);
 }
