64074ead3d5030cd0b4d036d4ceb6c78bc615adc
Luciano Holz <luciano.holz@hotmail.com>
2021-10-28 17:43:31 -0300

    Add changes on fields

--- src/app/validation/people/create.js
+++ src/app/validation/people/create.js
@@ -1,5 +1,5 @@
 
-const Joi = require('joi');
+const Joi = require('joi').extend(require('@joi/date'));
 
 module.exports = async (req, res, next) => {
     try {
@@ -9,18 +9,20 @@ module.exports = async (req, res, next) => {
             cpf: Joi.string()
                 .pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/))
                 .required(),
-            data_nascimento: Joi.date()
+            data_nascimento: Joi.date().format('DD/MM/YYYY')
                 .required(),
             email: Joi.string()
-                .required()
-                .email(),
+                .email()
+                .required(),
             senha: Joi.string()
+                .min(6)
                 .required(),
             habilitado: Joi.string()
+                .valid('sim', 'nao')
                 .required()
         });
-        
-        const { error } = await schema.validate(req.body, { abortEarly: true });
+       
+        const { error } = await schema.validate(req.body, { abortEarly: false });
         if(error) throw  error 
         return next();
 
