39aa15f190f4ffd624eb75ee27f32cb40da77b8a
Luciano Holz <luciano.holz@hotmail.com>
2021-10-28 17:49:12 -0300

    Adding changes

--- src/app/controller/AuthenticatorController.js
+++ src/app/controller/AuthenticatorController.js
@@ -14,12 +14,11 @@ class UserController {
             }
             
             const { _id, nome, cpf, data_nascimento, senha, createdAt, __v, ...authentication } = result.toObject();
-            function generateToken(params = {}) {
+            const  generateToken = (params = {}) => {
                 return jwt.sign(params, authConfig.secret, { expiresIn: 86400});
             }
-            
             return res.status(201)
-                .json({authentication, token: generateToken({id: authentication._id})});
+                .json({authentication, token: generateToken({id: authentication._id, email: authentication.email, habilitado: authentication.habilitado})});
 
         } catch (error) {
             return error;
