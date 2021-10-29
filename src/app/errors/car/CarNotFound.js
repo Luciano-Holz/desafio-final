a11b842d2615905ccad060ae4ca44559cb865ea2
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 19:57:20 -0300

    delete files

--- src/app/errors/car/CarNotFound.js
+++
@@ -1,9 +0,0 @@
-class CarNotFound extends Error {
-    constructor () {
-        super('Car not found.');
-        this.name = 'CarNotFound';
-        this.idErro = 2;
-    }
-}
-
-module.exports = CarNotFound;
--- src/app/errors/car/CarParamNotFound.js
+++
@@ -1,9 +0,0 @@
-class CarParamNotFound extends Error {
-    constructor () {
-        super('Car Params not found.');
-        this.name = 'CarParamNotFound';
-        this.idErro = 3;
-    }
-}
-
-module.exports = CarParamNotFound;
