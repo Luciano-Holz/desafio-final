c5788119aaf0a4c5a632f99518de2f513f5190eb
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 19:54:35 -0300

    Add validations on all methods

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -3,21 +3,25 @@ const CarService = require('../service/CarService');
 class CarController {
     async create(req, res) {
         const results = await CarService.create(req.body);
-        const  { modelo, cor, ano, acessorios, quantidadePassageiros } = results;
-        const result = { modelo, cor, ano, acessorios, quantidadePassageiros };
+        const  { _id, __v, ...result} = results.toObject();//
+        
         return res.status(201).json(result);
     }
     async getAll(req, res) {
-        const veiculos = await CarService.getAll(req.query);
-        return res.status(200).json({veiculos});
+        const result = await CarService.getAll(req.query);
+        const {pagingCounter, hasPrevPage, hasNextPage, prevPage, nextPage, ...veiculos} = result;
+        console.log(veiculos)
+        return res.status(200).json(veiculos);
     }
     async getById(req, res) {
         const results = await CarService.getById(req.params._id);
-        return res.status(200).json(results)
+        const  { __v, ...result} = results.toObject()
+        return res.status(200).json(result)
     }
     async update(req, res) {
         const { _id } = req.params;
-        const  result = await CarService.update(_id, req.body);
+        const  results = await CarService.update(_id, req.body);
+        const  { __v, ...result} = results.toObject()
         return res.status(200).json(result);
       
     }
