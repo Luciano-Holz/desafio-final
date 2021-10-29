8efa217e8e584e42605f0e0644a195fcad0f6da1
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 19:54:01 -0300

    Add validations on all methods

--- src/app/controller/PeopleController.js
+++ src/app/controller/PeopleController.js
@@ -1,39 +1,52 @@
 const PeopleService = require('../service/PeopleService');
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.json');
 
 class PeopleController {
     async create(req, res) {
         try {
             const result = await PeopleService.create(req.body);
-            const { senha, createdAt,  ...people } = result.toObject();
-            function generateToken(params = {}) {
-                return jwt.sign(params, authConfig.secret, { expiresIn: 86400});
-            }
-            return res.status(201)
-                .json({people, token: generateToken({id: people._id})});
+            const {_id, createdAt, __v,  ...people} = result.toObject();
+            return res.status(201).json({people});
         } catch (error) {
-            return res.status(400).json(error);
+            return res.status(400).json({message: error.message});
         }
-        
     }
     async getAll(req, res) {
-        const result = await PeopleService.getAll();
-        res.status(200).json(result);
+        try {
+            const result = await PeopleService.getAll();
+            const { createdAt, ...people} = result;
+            res.status(200).json(people);
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
     }
-    async getById(req, res) {
-        const result = await PeopleService.getById(req.params._id);
-        res.status(200).json(result);
+    async getById(req, res,) {
+        try {
+            const result = await PeopleService.getById(req.params._id);
+            const { createdAt, __v,  ...people} = result.toObject();
+            res.status(200).json(people);
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
     }
     async update(req, res) {
-        const { _id } = req.params;
-         await PeopleService.update(_id, req.body);
-        const result =await PeopleService.getById(_id);
-        res.status(200).json(result);
+        try {
+            const { _id } = req.params;
+            await PeopleService.update(_id, req.body);
+            const result = await PeopleService.getById(req.params._id);
+            const { createdAt, __v,  ...people} = result.toObject();
+            res.status(200).json(people);
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
+       
     }
     async delete(req, res) {
-        await PeopleService.delete(req.params._id);
-        res.status(204).end();
+        try {
+            await PeopleService.delete(req.params._id);
+            res.status(204).end();
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        } 
     }
 }
 
