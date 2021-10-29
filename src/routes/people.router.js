f46c0259deaa366aafb338496be05df8f0c31b63
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 19:22:57 -0300

    Add const IdValidation

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -1,12 +1,13 @@
 const PeopleController = require('../app/controller/PeopleController');
 const createValidation = require('../app/validation/people/create');
+const IdValidation = require('../app/validation/people/idValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/people') => {
     routes.post('/', createValidation, PeopleController.create);
     routes.get('/', PeopleController.getAll);
-    routes.get('/:_id', PeopleController.getById);
-    routes.put('/:_id', createValidation, PeopleController.update);
-    routes.delete('/:_id', PeopleController.delete);
+    routes.get('/:_id', IdValidation, PeopleController.getById);
+    routes.put('/:_id', IdValidation, createValidation,  PeopleController.update);
+    routes.delete('/:_id', IdValidation,   PeopleController.delete);
 
     server.use(prefix, routes);
 }
