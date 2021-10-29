af81bfc5337f0b430a623d967337756c0fff4798
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 19:26:39 -0300

    Add file IdValidation.js

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,14 +1,14 @@
 const CarController = require('../app/controller/CarController');
 const createValidation = require('../app/validation/car/create');
 //const authMiddleware = require('../app/middlewares/auth');
-const idValidation = require('../app/validation/car/idValidation');
+const IdValidation = require('../app/validation/car/IdValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/',createValidation , CarController.create);
     routes.get('/', CarController.getAll);
-    routes.get('/:_id', idValidation, CarController.getById); 
-    routes.put('/:_id', idValidation,  createValidation , CarController.update); 
-    routes.delete('/:_id', idValidation,  CarController.delete); 
+    routes.get('/:_id', IdValidation, CarController.getById); 
+    routes.put('/:_id', IdValidation,  createValidation , CarController.update); 
+    routes.delete('/:_id', IdValidation,  CarController.delete); 
   
     server.use(prefix, routes);
 }
