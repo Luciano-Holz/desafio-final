feb7ab3eee7f8e6bef87d02f22261a136bab75e8
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 22:59:44 -0300

    Add try catch

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -1,33 +1,50 @@
 const CarService = require('../service/CarService');
 
 class CarController {
-    async create(req, res) {
-        const results = await CarService.create(req.body);
-        const  { _id, __v, ...result} = results.toObject();//
-        
-        return res.status(201).json(result);
+    async create(req, res) { 
+        try {
+            const results = await CarService.create(req.body);
+            const  { _id, __v, ...result} = results.toObject();//
+            return res.status(201).json(result);
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        }
     }
     async getAll(req, res) {
-        const result = await CarService.getAll(req.query);
-        const {pagingCounter, hasPrevPage, hasNextPage, prevPage, nextPage, ...veiculos} = result;
-        console.log(veiculos)
-        return res.status(200).json(veiculos);
+        try {
+            const result = await CarService.getAll(req.query);
+            const {pagingCounter, hasPrevPage, hasNextPage, prevPage, nextPage, ...veiculos} = result;
+            return res.status(200).json({veiculos});
+        } catch (error) {
+            return res.status(400).json({message: error.message});
+        } 
     }
     async getById(req, res) {
-        const results = await CarService.getById(req.params._id);
-        const  { __v, ...result} = results.toObject()
-        return res.status(200).json(result)
+        try {
+            const results = await CarService.getById(req.params._id);
+            const  { __v, ...result} = results.toObject();
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json('Id Invalid');
+        }
     }
     async update(req, res) {
-        const { _id } = req.params;
-        const  results = await CarService.update(_id, req.body);
-        const  { __v, ...result} = results.toObject()
-        return res.status(200).json(result);
-      
+        try {
+             const { _id } = req.params;
+            const  results = await CarService.update(_id, req.body);
+            const  { __v, ...result} = results.toObject();
+            return res.status(200).json(result);
+        } catch (error) {
+            return res.status(400).json('Id Invalid');
+        }
     }
     async delete(req, res) {
-        await CarService.delete(req.params._id);
-        return res.status(204).end();
+        try {
+           const result = await CarService.delete(req.params._id);
+            return res.status(204).end(); 
+        } catch (error) {
+            return res.status(400).json('Id Invalid');
+        }  
     }
 }
 
