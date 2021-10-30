3346fb70f668b42cdc8d808a5dc4a968007e521a
Luciano Holz <luciano.holz@hotmail.com>
2021-10-29 22:55:27 -0300

    Remove try catch

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -2,44 +2,25 @@ const CarRepository = require('../repository/CarRepository');
 
 class CarService {
     async create(payload) {
-        try {
             const result = await CarRepository.create(payload);
             return result;
-        } catch(error) {
-            return error;
-        }
     }
     async getAll(queryParams) {
-        try {
             const result = await CarRepository.getAll(queryParams);
             return result;
-        } catch(error) {
-            return error;
-        }
     }
     async getById(id) {
-        try {
             const result = await CarRepository.getById(id);
             return result;
-        } catch(error) {
-            return error;
-        }
+       
     }
     async update(id, payload) {
-        try {
             const result = await CarRepository.update(id, payload);
             return result;
-        } catch(error) {
-            return error;
-        }
     }
     async delete(id) {
-        try {
             const result = await CarRepository.delete(id);
-            return result;
-        } catch(error) {
-            return error;
-        }
+            return result;    
     }
 }
 
