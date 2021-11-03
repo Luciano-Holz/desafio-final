83cc2e28e58fd8f564f73edf5a4800441dc17734
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 17:07:24 -0300

    Change because eslint

--- src/app/controller/AuthenticatorController.js
+++ src/app/controller/AuthenticatorController.js
@@ -1,30 +1,30 @@
-const UserService = require('../service/AuthenticatorService');
 const jwt = require('jsonwebtoken');
+const UserService = require('../service/AuthenticatorService');
 
 const authConfig = require('../config/auth.json');
 
-
 class UserController {
-    async auth(req, res) {
-        
-        try {
-            const result = await UserService.auth(req.body);
-            if(!result) {
-                return res.status(404).json('Not found authentication')
-            }
-            
-            const { _id, nome, cpf, data_nascimento, senha, createdAt, __v, ...authentication } = result.toObject();
-            const  generateToken = (params = {}) => {
-                return jwt.sign(params, authConfig.secret, { expiresIn: 86400});
-            }
-            return res.status(201)
-                .json({authentication, token: generateToken({id: authentication._id, email: authentication.email, habilitado: authentication.habilitado})});
+  async auth(req, res) {
+    try {
+      const result = await UserService.auth(req.body);
+      if (!result) {
+        return res.status(404).json('Not found authentication');
+      }
 
-        } catch (error) {
-            return error;
-        }
-       
+      const { _id, nome, cpf, data_nascimento, senha, createdAt, __v, ...authentication } = result.toObject();
+      const generateToken = (params = {}) => jwt.sign(params, authConfig.secret, { expiresIn: 86400 });
+      return res.status(201).json({
+        authentication,
+        token: generateToken({
+          id: authentication._id,
+          email: authentication.email,
+          habilitado: authentication.habilitado
+        })
+      });
+    } catch (error) {
+      return error;
     }
+  }
 }
 
 module.exports = new UserController();
