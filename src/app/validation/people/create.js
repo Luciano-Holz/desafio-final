b097aebb58ece50fffa1298f4b70222a04d5bdea
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:59:46 -0300

    Change because eslint

--- src/app/validation/people/create.js
+++ src/app/validation/people/create.js
@@ -1,33 +1,30 @@
-
 const Joi = require('joi').extend(require('@joi/date'));
 
 module.exports = async (req, res, next) => {
-    try {
-        const schema = Joi.object({
-            nome: Joi.string()
-                .trim()
-                .required(),
-            cpf: Joi.string()
-                .pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/))             
-                .required(),
-            data_nascimento: Joi.date().format('DD/MM/YYYY')
-                .required(),
-            email: Joi.string()
-                .email()
-                .required(),
-            senha: Joi.string()
-                .min(6)
-                .required(),
-            habilitado: Joi.string()
-                .valid('sim', 'nao')
-                .required()
-        });
-       
-        const { error } = await schema.validate(req.body, { abortEarly: false });
-        if(error) throw  error 
-        return next();
+  try {
+    const schema = Joi.object({
+      nome: Joi.string().trim().min(3).required(),
+      cpf: Joi.string()
+        .pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/))
+        .required(),
+      data_nascimento: Joi.date().format('DD/MM/YYYY').required(),
+      email: Joi.string().email().required(),
+      senha: Joi.string().min(6).required(),
+      habilitado: Joi.string().valid('sim', 'nao').required()
+    });
 
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    const err = [];
+    const { details } = error;
+    details.forEach((e) => {
+      err.push({
+        description: e.path[0],
+        name: e.message
+      });
+    });
+    return res.status(400).json(err);
+  }
+};
