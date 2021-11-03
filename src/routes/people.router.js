a75874d670f707530cf5e95f2b672345204c664d
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:33:36 -0300

    Change because eslint

--- src/routes/people.router.js
+++ src/routes/people.router.js
@@ -1,13 +1,13 @@
 const PeopleController = require('../app/controller/PeopleController');
+const IdValidation = require('../app/validation/car/IdValidation');
 const createValidation = require('../app/validation/people/create');
-const IdValidation = require('../app/validation/people/IdValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/people') => {
-    routes.post('/', createValidation, PeopleController.create);
-    routes.get('/', PeopleController.getAll);
-    routes.get('/:_id', IdValidation, PeopleController.getById);
-    routes.put('/:_id', IdValidation, createValidation,  PeopleController.update);
-    routes.delete('/:_id', IdValidation,   PeopleController.delete);
+  routes.post('/', createValidation, PeopleController.create);
+  routes.get('/', PeopleController.getAll);
+  routes.get('/:_id', IdValidation, PeopleController.getById);
+  routes.put('/:_id', IdValidation, createValidation, PeopleController.update);
+  routes.delete('/:_id', IdValidation, PeopleController.delete);
 
-    server.use(prefix, routes);
-}
+  server.use(prefix, routes);
+};
