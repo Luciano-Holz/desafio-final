07b19581e3f84fe790a399a98440e9200cc20102
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:29:17 -0300

    Change because eslint

--- src/routes/authenticator.router.js
+++ src/routes/authenticator.router.js
@@ -1,6 +1,6 @@
 const UserController = require('../app/controller/AuthenticatorController');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
-    routes.post('/', UserController.auth);
-    server.use(prefix, routes);
-}
+  routes.post('/', UserController.auth);
+  server.use(prefix, routes);
+};
