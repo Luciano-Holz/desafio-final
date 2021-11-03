d96faf0f21b550e66ab77ac22ccced69036e03d6
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 18:36:40 -0300

    Change because eslint

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -1,27 +1,32 @@
+const CarErrors = require('../errors/car/CarErrors');
 const CarRepository = require('../repository/CarRepository');
 
 class CarService {
-    async create(payload) {
-            const result = await CarRepository.create(payload);
-            return result;
-    }
-    async getAll(queryParams) {
-            const result = await CarRepository.getAll(queryParams);
-            return result;
-    }
-    async getById(id) {
-            const result = await CarRepository.getById(id);
-            return result;
-       
-    }
-    async update(id, payload) {
-            const result = await CarRepository.update(id, payload);
-            return result;
-    }
-    async delete(id) {
-            const result = await CarRepository.delete(id);
-            return result;    
-    }
+  async create(payload) {
+    const result = await CarRepository.create(payload);
+    return result;
+  }
+
+  async getAll(queryParams) {
+    const result = await CarRepository.getAll(queryParams);
+    return result;
+  }
+
+  async getById(id) {
+    const result = await CarRepository.getById(id);
+    if (!result) throw new CarErrors();
+    return result;
+  }
+
+  async update(id, payload) {
+    const result = await CarRepository.update(id, payload);
+    return result;
+  }
+
+  async delete(id) {
+    const result = await CarRepository.delete(id);
+    return result;
+  }
 }
 
 module.exports = new CarService();
