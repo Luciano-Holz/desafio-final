7104d3070729dbc8e9873705c649af34f9f644eb
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 17:05:44 -0300

    Change because eslint

--- src/app/serialize/CarSerielize.js
+++ src/app/serialize/CarSerielize.js
@@ -1,15 +1,18 @@
-const serialize = ({_id, modelo, cor, ano, acessorios, quantidadePassageiros}) => {
-    return {_id, modelo, cor, ano, acessorios, quantidadePassageiros}
-}
+const serialize = ({ _id, modelo, cor, ano, acessorios, quantidadePassageiros }) => ({
+  _id,
+  modelo,
+  cor,
+  ano,
+  acessorios,
+  quantidadePassageiros
+});
 
-const paginateSerialize = ({docs, limit, pagingCounter, totalDocs, totalPages}) => {
-    return {
-        veiculos: docs.map(serialize), 
-        limit, 
-        total: totalDocs,
-        offset: pagingCounter, 
-        offsets: totalPages
-    }
-} 
+const paginateSerialize = ({ docs, limit, pagingCounter, totalDocs, totalPages }) => ({
+  veiculos: docs.map(serialize),
+  limit,
+  total: totalDocs,
+  offset: pagingCounter,
+  offsets: totalPages
+});
 
-module.exports = {serialize, paginateSerialize}
+module.exports = { serialize, paginateSerialize };
