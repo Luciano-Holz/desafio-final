0389b3914f5d38f01fffaf4301ecaea51c083aa9
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:38:07 -0300

    Change because eslint

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,23 +2,19 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
 
 module.exports = (req, res, next) => {
-    const authReader = req.headers.authorization;
-    if(!authReader) 
-        return res.status(401).send({error: 'No token provided.'} );
+  const authReader = req.headers.authorization;
+  if (!authReader) return res.status(401).send({ error: 'No token provided.' });
 
-    const parts = authReader.split(' ');
-    if(!parts.length === 2)
-        return res.status(401).send({error: 'Token error.'});
+  const parts = authReader.split(' ');
+  if (!parts.length === 2) return res.status(401).send({ error: 'Token error.' });
 
-    const [ scheme, token ] = parts;
-    if(!/^Bearer$/i.test(scheme)) 
-        return res.status(401).send({error: 'Token malformatted'});
-    
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-        if(err) return res.status(401).send({error: 'Token Invalid'});
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme)) return res.status(401).send({ error: 'Token malformatted' });
 
-        req.userId = decoded._id;
-        return next();
-    })
-    
-}
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) return res.status(401).send({ error: 'Token Invalid' });
+
+    req.userId = decoded._id;
+    return next();
+  });
+};
