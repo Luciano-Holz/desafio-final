5f7328877a0337dfb0284159396d9504cdbb1516
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:34:42 -0300

    Change because eslint

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 class Database {
-    constructor() {
-        this.connect();
-    }
+  constructor() {
+    this.connect();
+  }
 
-    connect() {
-        return mongoose.connect('mongodb://localhost:27017/final_challenge');
-    }
+  connect() {
+    return mongoose.connect('mongodb://localhost:27017/final_challenge');
+  }
 }
 
 module.exports = new Database().connect();
