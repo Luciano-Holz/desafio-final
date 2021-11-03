1f21ae80f82105234d1c991c5a46ce8fd39e35e7
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 16:42:26 -0300

    Change because eslint

--- src/app/schema/PeopleSchema.js
+++ src/app/schema/PeopleSchema.js
@@ -2,45 +2,41 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 
 const PeopleSchema = mongoose.Schema({
-    nome: {
-        type: String,
-        required: true
-    },
-    cpf: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    data_nascimento: {
-        type: Date,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        lowercase: true
-    },
-    senha: {
-        type: String,
-        required: true,
-        select: false,
-        set: value => 
-            crypto
-            .createHash('md5')
-            .update(value)
-            .digest('hex')
-    },
-    habilitado: {
-        type: String,
-        required: true,
-        enum: ['sim', 'nao']
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+  nome: {
+    type: String,
+    required: true
+  },
+  cpf: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  data_nascimento: {
+    type: Date,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  senha: {
+    type: String,
+    required: true,
+    select: false,
+    set: (value) => crypto.createHash('md5').update(value).digest('hex')
+  },
+  habilitado: {
+    type: String,
+    required: true,
+    enum: ['sim', 'nao']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const People = mongoose.model('People', PeopleSchema);
 
