a807a6f4af9d4ef3ac0dfcb12b93b7b41d7ae3d4
Luciano Holz <luciano.holz@hotmail.com>
2021-11-03 18:33:15 -0300

    Add error handling in catch

--- src/app/validation/car/create.js
+++ src/app/validation/car/create.js
@@ -1,27 +1,27 @@
 const Joi = require('joi');
 
 module.exports = async (req, res, next) => {
-    try {
-        const schema = Joi.object({
-            modelo: Joi.string()
-                .required(),
-            cor: Joi.string()
-                .required(),
-            ano: Joi.number()
-                .integer()
-                .min(1950)
-                .max(2022)
-                .required(),
-            acessorios: Joi.array().items({descricao: Joi.string().required()}).unique(),
-            quantidadePassageiros: Joi.number()
-                .required()
-            
-        });
+  try {
+    const schema = Joi.object({
+      modelo: Joi.string().trim().required(),
+      cor: Joi.string().trim().required(),
+      ano: Joi.number().integer().min(1950).max(2022).required(),
+      acessorios: Joi.array().items({ descricao: Joi.string().trim().required() }).unique(),
+      quantidadePassageiros: Joi.number().required()
+    });
 
-        const { error } = await schema.validate(req.body, { abortEarly: true });
-        if(error) throw  error 
-        return next();
-    } catch(error) {
-        return res.status(400).json(error);
-    }
-}
+    const { error } = await schema.validate(req.body, { abortEarly: true });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    const err = [];
+    const { details } = error;
+    details.forEach((e) => {
+      err.push({
+        description: e.path[0],
+        name: e.message
+      });
+    });
+    return res.status(400).json(err);
+  }
+};
