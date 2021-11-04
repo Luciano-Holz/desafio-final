8bb5527af51e5f1c6abad3b70b902d058f9eb33f
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 14:10:21 -0300

    Change because eslint

--- src/app/repository/CarRepository.js
+++ src/app/repository/CarRepository.js
@@ -1,29 +1,37 @@
-
 const CarSchema = require('../schema/CarSchema');
 
 class CarRepository {
-    async create(payload) {
-        return CarSchema.create(payload);
-    }
-    async getAll(queryParams) {
-        const { page = 1, limit = 100, ...query } = queryParams;
-        return await CarSchema.paginate(
-            {...query}, 
-        {
-            limit: Number(limit), 
-            page: Number(page),
-            skip: (Number(page) -1) * Number(limit)
-        });
-    }
-    async getById(_id) {
-        return await CarSchema.findById({_id});
-    }
-    async update(_id, payload) {
-        return await CarSchema.findByIdAndUpdate({_id}, payload);
-    }
-    async delete(_id) {
-        return await CarSchema.findByIdAndDelete({_id});
-    }
+  async create(payload) {
+    return CarSchema.create(payload);
+  }
+
+  async getAll(queryParams) {
+    const { page = 1, limit = 100, ...query } = queryParams;
+    const result = await CarSchema.paginate(
+      { ...query },
+      {
+        limit: Number(limit),
+        page: Number(page),
+        skip: (Number(page) - 1) * Number(limit)
+      }
+    );
+    return result;
+  }
+
+  async getById(_id) {
+    const result = await CarSchema.findById({ _id });
+    return result;
+  }
+
+  async update(_id, payload) {
+    const result = await CarSchema.findByIdAndUpdate({ _id }, payload);
+    return result;
+  }
+
+  async delete(_id) {
+    const result = await CarSchema.findByIdAndDelete({ _id });
+    return result;
+  }
 }
 
 module.exports = new CarRepository();
