6c049621d15b52076a5bb2da97f2047cebc257f2
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 15:28:59 -0300

    Add throw new CarNotFound

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -1,30 +1,36 @@
-const CarErrors = require('../errors/car/CarErrors');
 const CarRepository = require('../repository/CarRepository');
+const CarNotFound = require('../errors/car/CarNotFound');
 
 class CarService {
   async create(payload) {
     const result = await CarRepository.create(payload);
+    if (!result) throw new CarNotFound();
     return result;
   }
 
   async getAll(queryParams) {
     const result = await CarRepository.getAll(queryParams);
+    if (!result) throw new CarNotFound();
     return result;
   }
 
-  async getById(id) {
-    const result = await CarRepository.getById(id);
-    if (!result) throw new CarErrors();
+  async getById(_id) {
+    const result = await CarRepository.getById(_id);
+    if (!result) throw new CarNotFound(_id);
     return result;
   }
 
-  async update(id, payload) {
-    const result = await CarRepository.update(id, payload);
+  async update(_id, payload) {
+    const result = await CarRepository.update(_id, payload);
+    if (!result) throw new CarNotFound(_id);
     return result;
   }
 
-  async delete(id) {
-    const result = await CarRepository.delete(id);
+  async delete(_id) {
+    const result = await CarRepository.delete(_id);
+    if (!result) {
+      throw new CarNotFound(_id);
+    }
     return result;
   }
 }
