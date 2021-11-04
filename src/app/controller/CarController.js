cc5fe8c41b630841ef2a45aa05290ec3f4a89e54
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 15:30:09 -0300

    Add description on res.status

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -7,7 +7,7 @@ class CarController {
       const result = await CarService.create(req.body);
       return res.status(201).json(serialize(result));
     } catch (error) {
-      return res.status(400).json({ message: error.message });
+      return res.status(400).json({ description: error.path, error: error.message });
     }
   }
 
@@ -16,7 +16,7 @@ class CarController {
       const result = await CarService.getAll(req.query);
       return res.status(200).json(paginateSerialize(result));
     } catch (error) {
-      return res.status(400).json({ message: error.message });
+      return res.status(400).json({ description: error.path, error: error.message });
     }
   }
 
@@ -25,7 +25,7 @@ class CarController {
       const result = await CarService.getById(req.params._id);
       return res.status(200).json(serialize(result));
     } catch (error) {
-      return res.status(400).json({ message: error.message });
+      return res.status(400).json({ description: error.path, error: error.message });
     }
   }
 
@@ -35,7 +35,7 @@ class CarController {
       const result = await CarService.update(_id, req.body);
       return res.status(200).json(serialize(result));
     } catch (error) {
-      return res.status(400).json({ message: error.message });
+      return res.status(400).json({ description: error.path, error: error.message });
     }
   }
 
@@ -44,7 +44,8 @@ class CarController {
       await CarService.delete(req.params._id);
       return res.status(204).end();
     } catch (error) {
-      return res.status(400).json({ message: error.message });
+      if (error.idErro === 2) return res.status(404).json({ description: error.path, name: error.message });
+      return res.status(400).json({ description: error.path, error: error.message });
     }
   }
 }
