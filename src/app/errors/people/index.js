78cfa4369940a88a3c85bf25c02378dba0968c39
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 14:16:08 -0300

    Change because eslint

--- src/app/errors/people/index.js
+++ src/app/errors/people/index.js
@@ -1,17 +1,17 @@
-
-const IdInvalid = require('./PeopleIdInvalid')
+const IdInvalid = require('./PeopleIdInvalid');
 const PeopleNotFound = require('./PeopleNotFound');
 const PeopleParamNotFound = require('./PeopleParamNotFound');
 
-module.exports = async(error, req, res, next) => {
-    let statusCode = 500;
+module.exports = async (error, req, res, next) => {
+  let statusCode = 500;
 
-    if(error instanceof PeopleParamNotFound || error instanceof PeopleNotFound) {
-        statusCode = 404;
-    }
+  if (error instanceof PeopleParamNotFound || error instanceof PeopleNotFound) {
+    statusCode = 404;
+  }
 
-    if(error instanceof IdInvalid) {
-        statusCode = 400;
-    }
-    res.status(statusCode).send(JSON.stringify({ message: error.message, Error_id: error.idError }));
-}
+  if (error instanceof IdInvalid) {
+    statusCode = 400;
+  }
+  res.status(statusCode).send(JSON.stringify({ message: error.message, Error_id: error._idError }));
+  next();
+};
