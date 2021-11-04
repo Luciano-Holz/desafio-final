dcceea4e37ec64e0b3cf5a550c28d65775d6f94d
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 14:00:30 -0300

    Change because eslint

--- src/app/service/AuthenticatorService.js
+++ src/app/service/AuthenticatorService.js
@@ -1,10 +1,10 @@
 const PeopleRepository = require('../repository/PeopleRepository');
 
 class AuthenticatorService {
-    async auth(payload) {
-            const result = await PeopleRepository.auth(payload);
-            return result;
-    }
+  async auth(payload) {
+    const result = await PeopleRepository.auth(payload);
+    return result;
+  }
 }
 
 module.exports = new AuthenticatorService();
