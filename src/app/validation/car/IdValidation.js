acc283842ad9623999bc631fc8f6ab9ed006f748
Luciano Holz <luciano.holz@hotmail.com>
2021-11-04 14:12:32 -0300

    Change because eslint

--- src/app/validation/car/IdValidation.js
+++ src/app/validation/car/IdValidation.js
@@ -1,17 +1,15 @@
 const Joi = require('joi');
 
 module.exports = async (req, res, next) => {
-    try {
-        const schema = Joi.object({
-            _id: Joi.string()
-                .id()
-                .required(), 
-        });
+  try {
+    const schema = Joi.object({
+      _id: Joi.string().id().required()
+    });
 
-        const { error } = await schema.validate(req.params, { abortEarly: true });
-        if(error) throw  error 
-        return next();
-    } catch(error) {
-        return res.status(400).json(error);
-    }
-}
+    const { error } = await schema.validate(req.params, { abortEarly: true });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
