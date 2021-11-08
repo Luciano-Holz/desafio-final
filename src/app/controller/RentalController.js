dea8202fc7e9cee9007dd19b8130ef5c818f1c7c
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 14:47:26 -0300

    Add getById method

--- src/app/controller/RentalController.js
+++ src/app/controller/RentalController.js
@@ -1,11 +1,11 @@
-const { paginateSerialize } = require('../serialize/RentalSerialize');
+const { paginateSerialize, serialize } = require('../serialize/RentalSerialize');
 const RentalService = require('../service/RentalService');
 
 class RentalController {
   async create(req, res) {
     try {
       const result = await RentalService.create(req.body);
-      return res.status(201).json(result);
+      return res.status(201).json(serialize(result));
     } catch (error) {
       return res.status(400).json({ description: error.path, name: error.message });
     }
@@ -15,6 +15,15 @@ class RentalController {
     try {
       const result = await RentalService.getAll(req.query);
       return res.status(200).json(paginateSerialize(result));
+    } catch (error) {
+      return res.status(400).json({ description: error.path, name: error.message });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const result = await RentalService.getById(req.params._id);
+      return res.status(200).json(serialize(result));
     } catch (error) {
       return res.status(400).json({ description: error.path, name: error.message });
     }
