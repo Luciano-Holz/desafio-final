e286d27bb742f5d20a04281508fe48969ad6322a
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 16:25:29 -0300

    Add delete route

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,11 +1,13 @@
 const RentalController = require('../app/controller/RentalController');
+const IdValidation = require('../app/validation/idValidation');
 const CreateValidation = require('../app/validation/rental/create');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
   routes.post('/', CreateValidation, RentalController.create);
   routes.get('/', RentalController.getAll);
-  routes.get('/:_id', RentalController.getById);
-  routes.put('/:_id', CreateValidation, RentalController.update);
+  routes.get('/:_id', IdValidation, RentalController.getById);
+  routes.put('/:_id', IdValidation, CreateValidation, RentalController.update);
+  routes.delete('/:_id', IdValidation, RentalController.delete);
 
   server.use(prefix, routes);
 };
