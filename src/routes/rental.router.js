7f21d680376ec8e835a3bd94ae025f0ae9a4227c
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 14:48:01 -0300

    Add get ById route

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -4,6 +4,7 @@ const createValidation = require('../app/validation/rental/create');
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
   routes.post('/', createValidation, RentalController.create);
   routes.get('/', RentalController.getAll);
+  routes.get('/:_id', RentalController.getById);
 
   server.use(prefix, routes);
 };
