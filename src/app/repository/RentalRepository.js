fc1fc6de89c55f9902082fd2f20c67d8d711f113
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 14:24:16 -0300

    Add getAll  method

--- src/app/repository/RentalRepository.js
+++ src/app/repository/RentalRepository.js
@@ -4,6 +4,19 @@ class RentalRepository {
   async create(payload) {
     return RentalSchema.create(payload);
   }
+
+  async getAll(queryParams) {
+    const { page = 1, limit = 100, ...query } = queryParams;
+    const result = await RentalSchema.paginate(
+      { ...query },
+      {
+        limit: Number(limit),
+        page: Number(page),
+        skip: (Number(page) - 1) * Number(limit)
+      }
+    );
+    return result;
+  }
 }
 
 module.exports = new RentalRepository();
