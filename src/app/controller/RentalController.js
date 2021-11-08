7a821f48c7369c960b5256ac64c0e85554f85d77
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 14:25:51 -0300

    Add getAll  method

--- src/app/controller/RentalController.js
+++ src/app/controller/RentalController.js
@@ -1,3 +1,4 @@
+const { paginateSerialize } = require('../serialize/RentalSerialize');
 const RentalService = require('../service/RentalService');
 
 class RentalController {
@@ -5,6 +6,15 @@ class RentalController {
     try {
       const result = await RentalService.create(req.body);
       return res.status(201).json(result);
+    } catch (error) {
+      return res.status(400).json({ description: error.path, name: error.message });
+    }
+  }
+
+  async getAll(req, res) {
+    try {
+      const result = await RentalService.getAll(req.query);
+      return res.status(200).json(paginateSerialize(result));
     } catch (error) {
       return res.status(400).json({ description: error.path, name: error.message });
     }
