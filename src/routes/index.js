f79ade86932f8ea83245a65648db3a22917a2a19
Luciano Holz <luciano.holz@hotmail.com>
2021-11-08 10:32:15 -0300

    Add const rental and swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ const { Router } = require('express');
 const car = require('./car.router');
 const people = require('./people.router');
 const auth = require('./authenticator.router');
+const rental = require('./rental.router');
+const swagger = require('./swagger.router');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
     car(server, new Router());
     people(server, new Router());
     auth(server, new Router());
+    rental(server, new Router());
+    swagger(server, new Router());
     next();
   });
 };
