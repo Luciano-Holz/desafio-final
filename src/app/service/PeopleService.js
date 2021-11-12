3c6c4b5cf2727d4bb6e0bec60d00bf23cf4b4471
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 09:42:26 -0300

    Add check email and cpf in create and update methods

--- src/app/service/PeopleService.js
+++ src/app/service/PeopleService.js
@@ -3,6 +3,8 @@ const PeopleRepository = require('../repository/PeopleRepository');
 const PeopleIdInvalid = require('../errors/people/PeopleIdInvalid');
 const PeopleNotFound = require('../errors/people/PeopleNotFound');
 const { validateCpf } = require('../utils/cpfValidator');
+const CpfIsRegisted = require('../errors/people/CpfIsregisted');
+const EmailIsRegisted = require('../errors/people/EmailIsregisted');
 
 class PeopleService {
   async create(payload) {
@@ -11,6 +13,14 @@ class PeopleService {
     const dataT = moment().diff(formatData, 'years');
     if (dataT < 18) throw Error('Age under 18 years');
     payload.data_nascimento = formatData;
+    const checkCpf = await PeopleRepository.getAll({ cpf: payload.cpf });
+    if (checkCpf.docs.length > 0) {
+      throw new CpfIsRegisted();
+    }
+    const checkEmail = await PeopleRepository.getAll({ email: payload.email });
+    if (checkEmail.docs.length > 0) {
+      throw new EmailIsRegisted();
+    }
     const results = await PeopleRepository.create(payload);
     if (!results) throw new PeopleNotFound();
     return results;
@@ -29,44 +39,19 @@ class PeopleService {
   }
 
   async update(_id, payload) {
-    const cpfnew = payload.cpf;
-    const cpf = cpfnew.replace(/\.|-/g, '');
-    function validateCPF(cpF) {
-      const cpfs = cpf.replace(/[^\d]+/g, '');
-      if (cpfs === '') return false;
-      if (
-        cpF.length !== 11 ||
-        cpF === '00000000000' ||
-        cpF === '11111111111' ||
-        cpF === '22222222222' ||
-        cpF === '33333333333' ||
-        cpF === '44444444444' ||
-        cpF === '55555555555' ||
-        cpF === '66666666666' ||
-        cpF === '77777777777' ||
-        cpF === '88888888888' ||
-        cpF === '99999999999'
-      )
-        return false;
-      let add = 0;
-      for (let i = 0; i < 9; i++) add += parseInt(cpf.charAt(i), 10) * (10 - i);
-      let rev = 11 - (add % 11);
-      if (rev === 10 || rev === 11) rev = 0;
-      if (rev !== parseInt(cpf.charAt(9), 10)) return false;
-      add = 0;
-      for (let i = 0; i < 10; i++) add += parseInt(cpf.charAt(i), 10) * (11 - i);
-      rev = 11 - (add % 11);
-      if (rev === 10 || rev === 11) rev = 0;
-      if (rev !== parseInt(cpf.charAt(10), 10)) return false;
-      return true;
-    }
-    if (!validateCPF(cpf)) throw Error(`CPF ${cpf} is invalid`);
-    console.log(Error);
-    payload.cpf = cpfnew;
+    if (!validateCpf(payload)) throw Error(`CPF ${payload.cpf} is invalid`);
     const formatData = moment(payload.data_nascimento, 'DD/MM/YYYY').format('YYYY-MM-DD');
     const dataT = moment().diff(formatData, 'years');
     if (dataT < 18) throw Error('age under 18 years');
     payload.data_nascimento = formatData;
+    const checkCpf = await PeopleRepository.getAll({ cpf: payload.cpf });
+    if (checkCpf.docs.length > 0) {
+      throw new CpfIsRegisted();
+    }
+    const checkEmail = await PeopleRepository.getAll({ email: payload.email });
+    if (checkEmail.docs.length > 0) {
+      throw new EmailIsRegisted();
+    }
     const result = PeopleRepository.update(_id, payload);
     if (!result) throw new PeopleIdInvalid();
     return result;
