af042ff126600a7b0be0f7c19c6cbdb471bf65d8
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 08:54:02 -0300

    Add timestamps and collection true

--- src/app/schema/PeopleSchema.js
+++ src/app/schema/PeopleSchema.js
@@ -2,45 +2,47 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
-const PeopleSchema = mongoose.Schema({
-  nome: {
-    type: String,
-    required: true
+const PeopleSchema = mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true
+    },
+    cpf: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    data_nascimento: {
+      type: Date,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true
+    },
+    senha: {
+      type: String,
+      required: true,
+      select: false,
+      set: (value) => crypto.createHash('md5').update(value).digest('hex')
+    },
+    habilitado: {
+      type: String,
+      required: true,
+      enum: ['sim', 'nao']
+    }
   },
-  cpf: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  data_nascimento: {
-    type: Date,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true
-  },
-  senha: {
-    type: String,
-    required: true,
-    select: false,
-    set: (value) => crypto.createHash('md5').update(value).digest('hex')
-  },
-  habilitado: {
-    type: String,
-    required: true,
-    enum: ['sim', 'nao']
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true,
+    collection: 'people'
   }
-});
+);
 
 PeopleSchema.plugin(mongoosePaginate);
 
-const People = mongoose.model('People', PeopleSchema);
+const People = mongoose.model('people', PeopleSchema);
 
 module.exports = People;
