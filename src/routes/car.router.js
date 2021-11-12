417514514e42ed5c263ee090c823c4e0ee4f8533
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 09:19:31 -0300

    Add authMiddleware in all routes

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,16 +1,16 @@
 const CarController = require('../app/controller/CarController');
 const createValidation = require('../app/validation/car/create');
-// const authMiddleware = require('../app/middlewares/auth');
+const authMiddleware = require('../app/middlewares/auth');
 const IdValidation = require('../app/validation/idValidation');
 const descriptionValidation = require('../app/validation/car/descricaoValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-  routes.post('/', createValidation, CarController.create);
-  routes.get('/', CarController.getAll);
-  routes.get('/:_id', IdValidation, CarController.getById);
-  routes.put('/:_id', IdValidation, createValidation, CarController.update);
-  routes.delete('/:_id', IdValidation, CarController.delete);
-  routes.patch('/:_id/acessorios/:_idAcessorio', descriptionValidation, CarController.patch);
+  routes.post('/', authMiddleware, createValidation, CarController.create);
+  routes.get('/', authMiddleware, CarController.getAll);
+  routes.get('/:_id', authMiddleware, IdValidation, CarController.getById);
+  routes.put('/:_id', authMiddleware, IdValidation, createValidation, CarController.update);
+  routes.delete('/:_id', authMiddleware, IdValidation, CarController.delete);
+  routes.patch('/:_id/acessorios/:_idAcessorio', authMiddleware, descriptionValidation, CarController.patch);
 
   server.use(prefix, routes);
 };
