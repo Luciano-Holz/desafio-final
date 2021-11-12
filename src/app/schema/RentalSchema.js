77e19abf489cbc6c9e1d6fd09b7485f25f61e76c
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 08:55:50 -0300

    Add timestamps and collection

--- src/app/schema/RentalSchema.js
+++ src/app/schema/RentalSchema.js
@@ -1,56 +1,61 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
-const RentalSchema = mongoose.Schema({
-  nome: {
-    type: String,
-    required: true
-  },
-  cnpj: {
-    type: String,
-    unique: true
-  },
-  atividades: {
-    type: String,
-    required: true
-  },
-  endereco: [
-    {
-      cep: {
-        type: String,
-        unique: true,
-        required: true
-      },
-      logradouro: {
-        type: String
-      },
-      complemento: {
-        type: String,
-        required: false
-      },
-      bairro: {
-        type: String
-      },
-      number: {
-        type: Number,
-        required: true
-      },
-      localidade: {
-        type: String
-      },
-      uf: {
-        type: String
-      },
-      isFilial: {
-        type: Boolean,
-        required: true
+const RentalSchema = mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true
+    },
+    cnpj: {
+      type: String,
+      unique: true
+    },
+    atividades: {
+      type: String,
+      required: true
+    },
+    endereco: [
+      {
+        cep: {
+          type: String,
+          required: true
+        },
+        logradouro: {
+          type: String
+        },
+        complemento: {
+          type: String,
+          required: false
+        },
+        bairro: {
+          type: String
+        },
+        number: {
+          type: Number,
+          required: true
+        },
+        localidade: {
+          type: String
+        },
+        uf: {
+          type: String
+        },
+        isFilial: {
+          type: Boolean,
+          required: true
+        }
       }
-    }
-  ]
-});
+    ]
+  },
+  {
+    timestamps: true,
+    collection: 'rental'
+  }
+);
 
 RentalSchema.plugin(mongoosePaginate);
 
-const Rental = mongoose.model('Rental', RentalSchema);
+const Rental = mongoose.model('rental', RentalSchema);
 
 module.exports = Rental;
