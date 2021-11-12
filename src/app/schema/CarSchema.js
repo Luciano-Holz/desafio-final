2a2f0c42c961a58ec94efb2604c5a705a0111c8f
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 08:54:43 -0300

    Add timestamps and collection

--- src/app/schema/CarSchema.js
+++ src/app/schema/CarSchema.js
@@ -1,35 +1,41 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
-const CarSchema = mongoose.Schema({
-  modelo: {
-    type: String,
-    required: true
-  },
-  cor: {
-    type: String,
-    required: true
-  },
-  ano: {
-    type: Number,
-    required: true
-  },
-  acessorios: [
-    {
-      descricao: {
-        type: String,
-        required: true
+const CarSchema = mongoose.Schema(
+  {
+    modelo: {
+      type: String,
+      required: true
+    },
+    cor: {
+      type: String,
+      required: true
+    },
+    ano: {
+      type: Number,
+      required: true
+    },
+    acessorios: [
+      {
+        descricao: {
+          type: String,
+          required: true
+        }
       }
+    ],
+    quantidadePassageiros: {
+      type: Number,
+      required: true
     }
-  ],
-  quantidadePassageiros: {
-    type: Number,
-    required: true
+  },
+  {
+    timestamps: true,
+    collection: 'car'
   }
-});
+);
 
 CarSchema.plugin(mongoosePaginate);
 
-const Carro = mongoose.model('Car', CarSchema);
+const Carro = mongoose.model('car', CarSchema);
 
 module.exports = Carro;
