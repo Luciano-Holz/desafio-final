fd4464276d26a43362a099cb3ece41435bd62f93
Luciano Holz <luciano.holz@hotmail.com>
2021-11-12 20:18:10 -0300

    final adjustments

--- src/app/errors/people/PeopleParamNotFound.js
+++
@@ -1,9 +0,0 @@
-class PeopleParamNotFound extends Error {
-  constructor() {
-    super('Params not found.');
-    this.name = 'PeopleParamNotFound';
-    this.idErro = 3;
-  }
-}
-
-module.exports = PeopleParamNotFound;
