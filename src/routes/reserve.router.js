2020d4aa6a99a7a9c5b7c4d030a25cd06018c35e
Luciano Holz <luciano.holz@hotmail.com>
2021-11-17 18:00:03 -0300

    add get route

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -2,6 +2,7 @@ const ReserveController = require('../app/controller/ReserveController');
 
 module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
   routes.post('/', ReserveController.create);
+  routes.get('/', ReserveController.getAll);
 
   server.use(prefix, routes);
 };
