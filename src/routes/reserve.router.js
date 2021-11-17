782f809e8921bd605d61b55b3d58a8d47a432a98
Luciano Holz <luciano.holz@hotmail.com>
2021-11-17 18:09:15 -0300

    add getById route

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -3,6 +3,7 @@ const ReserveController = require('../app/controller/ReserveController');
 module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
   routes.post('/', ReserveController.create);
   routes.get('/', ReserveController.getAll);
+  routes.get('/:_id', ReserveController.getById);
 
   server.use(prefix, routes);
 };
