d67e6be07dc2bdf4dc26028e1435e91fb4cce85c
Luciano Holz <luciano.holz@hotmail.com>
2021-11-17 18:11:05 -0300

    add put route

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -4,6 +4,7 @@ module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
   routes.post('/', ReserveController.create);
   routes.get('/', ReserveController.getAll);
   routes.get('/:_id', ReserveController.getById);
+  routes.put('/:_id', ReserveController.update);
 
   server.use(prefix, routes);
 };
