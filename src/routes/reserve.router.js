56165696619902a734f45da6212283f300d08ea7
Luciano Holz <luciano.holz@hotmail.com>
2021-11-18 14:20:42 -0300

    change prefix

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,11 +1,11 @@
 const ReserveController = require('../app/controller/ReserveController');
 
-module.exports = (server, routes, prefix = '/api/v1/rental/:_id/reserve') => {
-  routes.post('/', ReserveController.create);
-  routes.get('/', ReserveController.getAll);
-  routes.get('/:_id', ReserveController.getById);
-  routes.put('/:_id', ReserveController.update);
-  routes.delete('/:_id', ReserveController.delete);
+module.exports = (server, routes, prefix = '/api/v1/rental/') => {
+  routes.post('/:_id/reserve', ReserveController.create);
+  routes.get('/:_id/reserve', ReserveController.getAll);
+  routes.get('/:_id/reserve/:_idReserve', ReserveController.getById);
+  routes.put('/:_id/reserve/:_idReserve', ReserveController.update);
+  routes.delete('/:_id/reserve/:_idReserve', ReserveController.delete);
 
   server.use(prefix, routes);
 };
