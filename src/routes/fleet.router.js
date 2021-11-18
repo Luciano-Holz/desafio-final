3f31aa18da4fa64e5ca972b4e80a5d30d78577d6
Luciano Holz <luciano.holz@hotmail.com>
2021-11-18 10:34:40 -0300

    add get route

--- src/routes/fleet.router.js
+++ src/routes/fleet.router.js
@@ -2,6 +2,7 @@ const FleetController = require('../app/controller/FleetController');
 
 module.exports = (server, routes, prefix = '/api/v1/rental/:_id/fleet') => {
   routes.post('/', FleetController.create);
+  routes.get('/', FleetController.getAll);
 
   server.use(prefix, routes);
 };
