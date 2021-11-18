54d662655fbf122ae7fe5cf7d643730c7539d01f
Luciano Holz <luciano.holz@hotmail.com>
2021-11-18 10:19:32 -0300

    add underscore on id

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,6 +1,6 @@
 const ReserveController = require('../app/controller/ReserveController');
 
-module.exports = (server, routes, prefix = '/api/v1/rental/:id/reserve') => {
+module.exports = (server, routes, prefix = '/api/v1/rental/:_id/reserve') => {
   routes.post('/', ReserveController.create);
   routes.get('/', ReserveController.getAll);
   routes.get('/:_id', ReserveController.getById);
