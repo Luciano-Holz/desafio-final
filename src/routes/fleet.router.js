4d615fbc9ee5ab9d1a446b3100dfb48a9f718a9f
Luciano Holz <luciano.holz@hotmail.com>
2021-11-18 10:54:10 -0300

    add getById route

--- src/routes/fleet.router.js
+++ src/routes/fleet.router.js
@@ -3,6 +3,7 @@ const FleetController = require('../app/controller/FleetController');
 module.exports = (server, routes, prefix = '/api/v1/rental/:_id/fleet') => {
   routes.post('/', FleetController.create);
   routes.get('/', FleetController.getAll);
+  routes.get('/:_id', FleetController.getById);
 
   server.use(prefix, routes);
 };
