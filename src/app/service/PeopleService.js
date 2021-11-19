c82f4c5f81d6bff9ac3725546c826c759b59c38e
Luciano Holz <luciano.holz@hotmail.com>
2021-11-19 11:05:19 -0300

    Changes on throw errors

--- src/app/service/PeopleService.js
+++ src/app/service/PeopleService.js
@@ -1,58 +1,58 @@
 const moment = require('moment');
 const PeopleRepository = require('../repository/PeopleRepository');
-const PeopleIdInvalid = require('../errors/people/PeopleIdInvalid');
-const PeopleNotFound = require('../errors/people/PeopleNotFound');
 const { validateCpf } = require('../utils/cpfValidator');
-const CpfIsRegisted = require('../errors/people/CpfIsregisted');
-const EmailIsRegisted = require('../errors/people/EmailIsregisted');
+const BadRequest = require('../errors/BadRequest');
+const NotFound = require('../errors/NotFound');
 
 class PeopleService {
   async create(payload) {
-    if (!validateCpf(payload)) throw Error(`CPF ${payload.cpf} is invalid`);
+    if (!validateCpf(payload)) throw new BadRequest('Conflict', `Cpf ${payload.cpf} is invalid`);
     const formatData = moment(payload.data_nascimento, 'DD/MM/YYYY').format('YYYY-MM-DD');
     const dataT = moment().diff(formatData, 'years');
-    if (dataT < 18) throw Error('Age under 18 years');
+    if (dataT < 18) throw BadRequest('data_nasimento', `Age under 18 years`);
     payload.data_nascimento = formatData;
+
     const checkCpf = await PeopleRepository.getAll({ cpf: payload.cpf });
     if (checkCpf.docs.length > 0) {
-      throw new CpfIsRegisted();
+      throw new BadRequest('Conflict', `Cpf ${payload.cpf} is already in use`);
     }
     const checkEmail = await PeopleRepository.getAll({ email: payload.email });
     if (checkEmail.docs.length > 0) {
-      throw new EmailIsRegisted();
+      throw new BadRequest('Conflict', `Email ${payload.email} is already in use`);
     }
     const results = await PeopleRepository.create(payload);
-    if (!results) throw new PeopleNotFound();
+    if (!results) throw new NotFound('People', 'not found');
     return results;
   }
 
   async getAll(queryParams) {
     const result = await PeopleRepository.getAll(queryParams);
-    if (!result) throw new PeopleNotFound();
+    if (!result) throw new NotFound('Params', 'not found');
     return result;
   }
 
   async getById(_id) {
     const result = await PeopleRepository.getById(_id);
-    if (!result) throw new PeopleIdInvalid(_id);
+    if (!result) throw new BadRequest('id', `Id ${_id} is invalid`);
     return result;
   }
 
   async update(_id, payload) {
-    if (!validateCpf(payload)) throw Error(`CPF ${payload.cpf} is invalid`);
+    if (!validateCpf(payload)) throw new BadRequest('Conflict', `Cpf ${payload.cpf} is invalid`);
     const formatData = moment(payload.data_nascimento, 'DD/MM/YYYY').format('YYYY-MM-DD');
     const dataT = moment().diff(formatData, 'years');
-    if (dataT < 18) throw Error('age under 18 years');
+    if (dataT < 18) throw BadRequest('data_nasimento', `Age under 18 years`);
     payload.data_nascimento = formatData;
+
     const result = PeopleRepository.update(_id, payload);
-    if (!result) throw new PeopleIdInvalid();
+    if (!result) throw new BadRequest('id', `Id ${_id} is invalid`);
     return result;
   }
 
   async delete(_id) {
     const result = await PeopleRepository.delete(_id);
     if (!result) {
-      throw new PeopleNotFound(_id);
+      throw new NotFound('id', `Id ${_id} not found`);
     }
     return result;
   }
