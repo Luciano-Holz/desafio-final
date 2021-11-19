03fc42996501e4464b4909ba22ae4d4a470b7128
Luciano Holz <luciano.holz@hotmail.com>
2021-11-19 18:01:45 -0300

    Add tests

--- __tests__/feature/people_test/peopleDelete.test.js
+++ __tests__/feature/people_test/peopleDelete.test.js
@@ -1,28 +1,36 @@
-// const request = require('supertest');
-// const app = require('../../../src/app');
+const request = require('supertest');
+const app = require('../../../src/app');
 
-// describe('API :: DELETE :: /people', () => {
-//   it('Should delete a people by Id', async () => {
-//     const fakerPeople = {
-//       nome: 'Fulano de Tal',
-//       cpf: '521.111.840-55',
-//       data_nascimento: '04/01/1995',
-//       senha: '123456',
-//       email: 'fulano@example.com',
-//       habilitado: 'sim'
-//     };
+let fakerPeople = {};
+describe('API :: DELETE :: /people', () => {
+  beforeEach(() => {
+    fakerPeople = {
+      nome: 'Fulano de Tal',
+      cpf: '521.111.840-55',
+      data_nascimento: '04/01/1995',
+      senha: '123456',
+      email: 'fulano@example.com',
+      habilitado: 'sim'
+    };
+  });
+  it('Should return an empty body', async () => {
+    const { text } = await request(app).post('/api/v1/people/').send(fakerPeople);
+    const { _id } = JSON.parse(text);
+    const response = await request(app).delete(`/api/v1/people/${_id.toString()}`);
+    const { body } = response;
 
-//     const { text } = await request(app).post('/api/v1/people/').send(fakerPeople);
-//     const { _id } = JSON.parse(text);
-//     const response = await request(app).delete(`/api/v1/people/${_id.toString()}`);
-//     const { body } = response;
+    expect(body.nome).toBeUndefined();
+    expect(body.cpf).toBeUndefined();
+    expect(body.data_nascimento).toBeUndefined();
+    expect(body.email).toBeUndefined();
+    expect(body.habilitado).toBeUndefined();
+  });
 
-//     expect(body.nome).toBeUndefined();
-//     expect(body.cpf).toBeUndefined();
-//     expect(body.data_nascimento).toBeUndefined();
-//     expect(body.email).toBeUndefined();
-//     expect(body.habilitado).toBeUndefined();
-//     const { status } = response;
-//     expect(status).toBe(204);
-//   });
-// });
+  it('Should return status code 204', async () => {
+    const { text } = await request(app).post('/api/v1/people/').send(fakerPeople);
+    const { _id } = JSON.parse(text);
+
+    const { status } = await request(app).delete(`/api/v1/people/${_id.toString()}`);
+    expect(status).toBe(204);
+  });
+});
