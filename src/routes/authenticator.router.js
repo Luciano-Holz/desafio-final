1260d45a6718dc47ee544c1d19647967a9fce656
Luciano Holz <luciano.holz@hotmail.com>
2021-11-20 11:43:54 -0300

    Add middleware AuthValidator

--- src/routes/authenticator.router.js
+++ src/routes/authenticator.router.js
@@ -1,6 +1,7 @@
 const UserController = require('../app/controller/AuthenticatorController');
+const AuthValidator = require('../app/validation/auth/create');
 
 module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
-  routes.post('/', UserController.auth);
+  routes.post('/', AuthValidator, UserController.auth);
   server.use(prefix, routes);
 };
