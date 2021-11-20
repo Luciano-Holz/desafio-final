8ecb1c35d8d0b676cf146e4b1473ba466ee7766e
Luciano Holz <luciano.holz@hotmail.com>
2021-11-20 11:06:08 -0300

    Remove validations

--- src/app/controller/AuthenticatorController.js
+++ src/app/controller/AuthenticatorController.js
@@ -1,28 +1,12 @@
-const jwt = require('jsonwebtoken');
 const UserService = require('../service/AuthenticatorService');
-
-const authConfig = require('../config/auth.json');
 
 class UserController {
   async auth(req, res) {
     try {
       const result = await UserService.auth(req.body);
-      if (!result) {
-        return res.status(404).json('Not found authentication');
-      }
-
-      const { _id, nome, cpf, data_nascimento, senha, createdAt, __v, ...authentication } = result.toObject();
-      const generateToken = (params = {}) => jwt.sign(params, authConfig.secret, { expiresIn: 86400 });
-      return res.status(200).json({
-        authentication,
-        token: generateToken({
-          id: authentication._id,
-          email: authentication.email,
-          habilitado: authentication.habilitado
-        })
-      });
-    } catch (error) {
-      return error;
+      return res.status(200).json(result);
+    } catch ({ name, description, status }) {
+      return res.status(status).json({ name, description });
     }
   }
 }
