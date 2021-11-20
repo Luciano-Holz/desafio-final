ce9b6a6750429707fccdd071f33f543b34b557ca
Luciano Holz <luciano.holz@hotmail.com>
2021-11-20 11:06:46 -0300

    Add validations

--- src/app/service/AuthenticatorService.js
+++ src/app/service/AuthenticatorService.js
@@ -1,9 +1,25 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const NotFound = require('../errors/NotFound');
 const PeopleRepository = require('../repository/PeopleRepository');
+
+dotenv.config();
 
 class AuthenticatorService {
   async auth(payload) {
-    const result = await PeopleRepository.auth(payload);
-    return result;
+    const { senha } = payload;
+    const people = await PeopleRepository.auth(payload.email);
+
+    if (!people) throw new NotFound('email', 'Email do not exists in database');
+
+    if (!(await bcrypt.compare(senha, people.senha))) throw new NotFound('senha', 'Senha is incorrect.');
+
+    const { email, habilitado } = people;
+
+    const token = jwt.sign({ email, habilitado }, process.env.API_SECRET, { expiresIn: 86400 });
+    const response = { email, habilitado, token };
+    return response;
   }
 }
 
