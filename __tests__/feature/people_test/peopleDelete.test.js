f87f830e0fa6f08c36a26bac0e33c51133500628
Luciano Holz <luciano.holz@hotmail.com>
2021-11-22 10:26:03 -0300

    add test id invalid

--- __tests__/feature/people_test/peopleDelete.test.js
+++ __tests__/feature/people_test/peopleDelete.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../../../src/app');
 
 let fakerPeople = {};
+let fakerId = '';
 describe('API :: DELETE :: /people', () => {
   beforeEach(() => {
     fakerPeople = {
@@ -32,5 +33,63 @@ describe('API :: DELETE :: /people', () => {
 
     const { status } = await request(app).delete(`/api/v1/people/${_id.toString()}`);
     expect(status).toBe(204);
+  });
+});
+
+describe('Should return an error id not found', () => {
+  beforeEach(() => {
+    fakerId = '6198fdf429f71a82ab732aa2';
+  });
+
+  it('Should return an object with name and description properties', async () => {
+    const { body } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(body.name).toBe('id');
+    expect(body.description).toBe(`Id ${fakerId} not found`);
+  });
+
+  it('should return a object with properties type string', async () => {
+    const { body } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(body).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+
+  it('Should return status code 404', async () => {
+    const { status } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(status).toBe(404);
+  });
+});
+
+describe('Should return an error id invalid', () => {
+  beforeEach(() => {
+    fakerId = '6198fdf429f71a82ab732a';
+  });
+
+  it('Should return an object with name and description properties', async () => {
+    const { body } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(body[0].name).toBe('_id');
+    expect(body[0].description).toBe(
+      `"_id" with value "${fakerId}" fails to match the required pattern: /^[0-9a-fA-F]{24}$/`
+    );
+  });
+
+  it('should return a object with properties type string', async () => {
+    const { body } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(body[0]).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+
+  it('Should return status code 404', async () => {
+    const { status } = await request(app).delete(`/api/v1/people/${fakerId}`);
+
+    expect(status).toBe(400);
   });
 });
