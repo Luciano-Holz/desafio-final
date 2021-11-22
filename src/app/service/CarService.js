8ae1f40eb330348ad8c35bfbe893d2575c11c660
Luciano Holz <luciano.holz@hotmail.com>
2021-11-22 10:31:49 -0300

    change errors handling

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -1,10 +1,11 @@
 const CarRepository = require('../repository/CarRepository');
-const CarNotFound = require('../errors/car/CarNotFound');
+const NotFound = require('../errors/NotFound');
+const BadRequest = require('../errors/BadRequest');
 
 class CarService {
   async create(payload) {
     const result = await CarRepository.create(payload);
-    if (!result) throw new CarNotFound();
+    if (!result) throw new NotFound('Car', 'not found');
     return result;
   }
 
@@ -14,31 +15,31 @@ class CarService {
       delete queryParams.descricao;
     }
     const result = await CarRepository.getAll(queryParams);
-    if (!result) throw new CarNotFound();
+    if (!result) throw new NotFound('Params', 'not found');
     return result;
   }
 
   async getById(_id) {
     const result = await CarRepository.getById(_id);
-    if (!result) throw new CarNotFound(_id);
+    if (!result) throw new BadRequest('id', `Id ${_id} is invalid`);
     return result;
   }
 
   async update(_id, payload) {
     const result = await CarRepository.update(_id, payload);
-    if (!result) throw new CarNotFound(_id);
+    if (!result) throw BadRequest('id', `Id ${_id} is invalid`);
     return result;
   }
 
   async delete(_id) {
     const result = await CarRepository.delete(_id);
-    if (!result) throw new CarNotFound(_id);
+    if (!result) throw new NotFound('id', `Id ${_id} not found`);
     return result;
   }
 
   async patch(_id, _idAcessorio, payload) {
     const result = await CarRepository.patch(_id, _idAcessorio, payload);
-    if (!result) throw new CarNotFound(_id);
+    if (!result) throw new NotFound('id', `Id ${_id} not found`);
     return result;
   }
 }
