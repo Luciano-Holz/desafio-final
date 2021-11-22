0a9a8c783ec5f2ee43db114581d54ccf2e394448
Luciano Holz <luciano.holz@hotmail.com>
2021-11-22 11:32:42 -0300

    add test create

--- __tests__/feature/car_test/carPost.test.js
+++ __tests__/feature/car_test/carPost.test.js
@@ -1,51 +1,81 @@
-// const request = require('supertest');
-// const app = require('../../../src/app');
+const request = require('supertest');
+const app = require('../../../src/app');
 
-// let BearerToken = null;
-// beforeAll(async () => {
-//   const fakerPeople = {
-//     nome: 'Fulano de Tal',
-//     cpf: '521.111.840-55',
-//     data_nascimento: '04/01/1995',
-//     senha: '1234567',
-//     email: 'fulano@example.com',
-//     habilitado: 'sim'
-//   };
-//   await request(app).post('/api/v1/people/').send(fakerPeople);
+let BearerToken = null;
 
-//   const result = await request(app)
-//     .post('/api/v1/authenticate/')
-//     .send({ email: fakerPeople.email, senha: fakerPeople.senha });
+beforeAll(async () => {
+  const fakerPeople = {
+    nome: 'Beltrano de Tal',
+    cpf: '255.655.954-36',
+    data_nascimento: '08/07/1999',
+    senha: 'senha123',
+    email: 'beltrano@example.com',
+    habilitado: 'sim'
+  };
+  await request(app).post('/api/v1/people/').send(fakerPeople);
 
-//   const { body } = result;
-//   BearerToken = body.token;
-// });
+  const result = await request(app)
+    .post('/api/v1/authenticate/')
+    .send({ email: fakerPeople.email, senha: fakerPeople.senha });
 
-// describe('API :: POST :: /car', () => {
-//   it('Should return Created status 201', async () => {
-//     const fakerCar = {
-//       modelo: 'Ford Focus',
-//       cor: 'Prata',
-//       ano: 2013,
-//       acessorios: [{ descricao: 'Ar-condicionado' }],
-//       quantidadePassageiros: 5
-//     };
-//     const { body } = await request(app)
-//       .post('/api/v1/car/')
-//       .set('Authorization', `Bearer ${BearerToken}`)
-//       .send(fakerCar);
-//     expect(body).toEqual({
-//       _id: expect.any(String),
-//       modelo: expect.any(String),
-//       cor: expect.any(String),
-//       ano: expect.any(Number),
-//       acessorios: expect.any(Array),
-//       quantidadePassageiros: expect.any(Number)
-//     });
-//     const { status } = await request(app)
-//       .post('/api/v1/car/')
-//       .set('Authorization', `Bearer ${BearerToken}`)
-//       .send(fakerCar);
-//     expect(status).toBe(201);
-//   });
-// });
+  const { body } = result;
+  BearerToken = body.token;
+});
+
+let fakerCar = {};
+
+describe('API :: POST :: /car', () => {
+  beforeEach(async () => {
+    fakerCar = {
+      modelo: 'Ford Focus',
+      cor: 'Prata',
+      ano: 2013,
+      acessorios: [{ descricao: 'Ar-condicionado' }],
+      quantidadePassageiros: 5
+    };
+  });
+
+  it('Sould return a body with _id and same properties from fakerCar except senha', async () => {
+    const { body } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(fakerCar)
+      .send(fakerCar);
+
+    expect(body).toHaveProperty('_id');
+    expect(body.modelo).toBe(fakerCar.modelo);
+    expect(body.cor).toBe(fakerCar.cor);
+    expect(body.ano).toBe(fakerCar.ano);
+    expect(body.acessorios[0].descricao).toBe(fakerCar.acessorios[0].descricao);
+    expect(body.quantidadePassageiros).toBe(fakerCar.quantidadePassageiros);
+  });
+
+  it('Should return a body with values type String, Number and Array', async () => {
+    const { body } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(fakerCar);
+
+    expect(body).toEqual({
+      _id: expect.any(String),
+      modelo: expect.any(String),
+      cor: expect.any(String),
+      ano: expect.any(Number),
+      acessorios: expect.any(Array),
+      quantidadePassageiros: expect.any(Number)
+    });
+    const { status } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(fakerCar);
+    expect(status).toBe(201);
+  });
+
+  it('Should return status code 201', async () => {
+    const { status } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(fakerCar);
+    expect(status).toBe(201);
+  });
+});
