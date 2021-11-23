4eba0ad2776cac848b0cf43691002850008e326e
Luciano Holz <luciano.holz@hotmail.com>
2021-11-23 18:58:48 -0300

    Refatoring tests

--- __tests__/feature/car_test/carGet.test.js
+++ __tests__/feature/car_test/carGet.test.js
@@ -1,20 +1,14 @@
 const request = require('supertest');
 const app = require('../../../src/app');
+const { PeopleDataFaker } = require('../../support/dataFaker');
 
 let BearerToken = null;
-
+let result = {};
 beforeAll(async () => {
-  const fakerPeople = {
-    nome: 'Luciano Holz',
-    cpf: '035.891.820-08',
-    data_nascimento: '07/06/2001',
-    email: 'luciano@example.com',
-    senha: '123456',
-    habilitado: 'sim'
-  };
+  const fakerPeople = PeopleDataFaker();
   await request(app).post('/api/v1/people/').send(fakerPeople);
 
-  const result = await request(app)
+  result = await request(app)
     .post('/api/v1/authenticate/')
     .send({ email: fakerPeople.email, senha: fakerPeople.senha });
 
@@ -42,14 +36,15 @@ describe('API :: GET :: /car', () => {
       acessorios: [{ descricao: 'Ar-condicionado' }, { descricao: 'Direção Hidraulica' }],
       quantidadePassageiros: 5
     };
-  });
 
-  it('Should get all cars', async () => {
     await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar);
     await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar2);
 
-    const response = await request(app).get('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`);
-    const { body } = response;
+    result = await request(app).get('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`);
+  });
+
+  it('Should get all cars', (done) => {
+    const { body } = result;
     const { veiculos } = body;
 
     expect(veiculos[0]).toHaveProperty('_id');
@@ -65,14 +60,11 @@ describe('API :: GET :: /car', () => {
     expect(veiculos[1].ano).toBe(fakerCar2.ano);
     expect(veiculos[1].acessorios[0].descricao).toBe(fakerCar2.acessorios[0].descricao);
     expect(veiculos[1].acessorios[1].descricao).toBe(fakerCar2.acessorios[1].descricao);
+    done();
   });
 
-  it('Should return a body Array veiculos with values type String Number and Array  ', async () => {
-    await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar);
-    await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar2);
-
-    const { body } = await request(app).get('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`);
-
+  it('Should return a body Array veiculos with values type String Number and Array  ', (done) => {
+    const { body } = result;
     const { veiculos } = body;
 
     expect(veiculos[0]).toEqual({
@@ -92,14 +84,64 @@ describe('API :: GET :: /car', () => {
       acessorios: expect.any(Array),
       quantidadePassageiros: expect.any(Number)
     });
+    done();
   });
 
-  it('Should return status code 200', async () => {
-    await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar);
-    await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar2);
-
-    const { status } = await request(app).get('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`);
+  it('Should return status code 200', (done) => {
+    const { status } = result;
 
     expect(status).toBe(200);
+    done();
+  });
+});
+
+describe('Should get all cars by their names', () => {
+  beforeEach(async () => {
+    fakerCar = {
+      modelo: 'Fiat Pulse',
+      cor: 'Branca',
+      ano: 2022,
+      acessorios: [{ descricao: 'Ar-condicionado' }, { descricao: 'Direção Elétrica' }],
+      quantidadePassageiros: 5
+    };
+
+    await request(app).post('/api/v1/car/').set('Authorization', `Bearer ${BearerToken}`).send(fakerCar);
+
+    result = await request(app).get(`/api/v1/car/?nome=${fakerCar.nome}`).set('Authorization', `Bearer ${BearerToken}`);
+  });
+
+  it('Should return a body with the same properties from the parameter ', (done) => {
+    const { body } = result;
+    const { veiculos } = body;
+
+    expect(veiculos[0]).toHaveProperty('_id');
+    expect(veiculos[0].modelo).toBe(fakerCar.modelo);
+    expect(veiculos[0].cor).toBe(fakerCar.cor);
+    expect(veiculos[0].ano).toBe(fakerCar.ano);
+    expect(veiculos[0].acessorios[0].descricao).toBe(fakerCar.acessorios[0].descricao);
+    expect(veiculos[0].acessorios[1].descricao).toBe(fakerCar.acessorios[1].descricao);
+    done();
+  });
+
+  it('Should return a body Array veiculos with values type String Number and Array  ', (done) => {
+    const { body } = result;
+    const { veiculos } = body;
+
+    expect(veiculos[0]).toEqual({
+      _id: expect.any(String),
+      modelo: expect.any(String),
+      cor: expect.any(String),
+      ano: expect.any(Number),
+      acessorios: expect.any(Array),
+      quantidadePassageiros: expect.any(Number)
+    });
+    done();
+  });
+
+  it('Should return status code 200', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(200);
+    done();
   });
 });
