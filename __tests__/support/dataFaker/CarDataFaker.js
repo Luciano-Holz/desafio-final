6462baaeeb476765cc61effca0956e7f383bd8c6
Luciano Holz <luciano.holz@hotmail.com>
2021-11-24 11:26:25 -0300

    Refatoring code CarDataFaker

--- __tests__/support/dataFaker/CarDataFaker.js
+++ __tests__/support/dataFaker/CarDataFaker.js
@@ -5,13 +5,16 @@ const generateCarJson = (count = 1) => {
   for (let index = 0; index < count; index++) {
     objs.push({
       modelo: dataFaker.name({ middle: true }),
-      cor: dataFaker.string({ length: 10, pool: 'abcdefghijlmnoprstuvxz' }),
-      ano: dataFaker.year({ min: 1950, max: 2022 }),
-      acessorios: dataFaker.array([{}]),
+      cor: dataFaker.color(),
+      ano: dataFaker.integer({ min: 1950, max: 2022 }),
+      acessorios: [
+        {
+          descricao: dataFaker.name({ middle: true })
+        }
+      ],
       quantidadePassageiros: dataFaker.natural({ min: 1, max: 7 })
     });
   }
   return count === 1 ? objs[0] : objs;
 };
-
 module.exports = generateCarJson;
