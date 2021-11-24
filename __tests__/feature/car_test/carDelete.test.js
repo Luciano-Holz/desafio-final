c278ff4df47d2756cf16c0031cb3cd523a5e8d9a
Luciano Holz <luciano.holz@hotmail.com>
2021-11-24 11:22:20 -0300

    Refatoring and add tests

--- __tests__/feature/car_test/carDelete.test.js
+++ __tests__/feature/car_test/carDelete.test.js
@@ -1,53 +1,119 @@
-// const request = require('supertest');
-// const app = require('../../../src/app');
+const request = require('supertest');
+const app = require('../../../src/app');
+const { PeopleDataFaker } = require('../../support/dataFaker');
+const { CarDataFaker } = require('../../support/dataFaker');
 
-// let BearerToken = null;
-// beforeAll(async () => {
-//   const fakerPeople = {
-//     nome: 'Luciano Holz',
-//     cpf: '035.891.820-08',
-//     data_nascimento: '07/06/2001',
-//     email: 'luciano@example.com',
-//     senha: '123456',
-//     habilitado: 'sim'
-//   };
-//   await request(app).post('/api/v1/people/').send(fakerPeople);
+let BearerToken = null;
+let fakerId = {};
+let result = {};
+beforeAll(async () => {
+  const fakerPeople = PeopleDataFaker();
+  await request(app).post('/api/v1/people/').send(fakerPeople);
 
-//   const result = await request(app)
-//     .post('/api/v1/authenticate/')
-//     .send({ email: fakerPeople.email, senha: fakerPeople.senha });
+  result = await request(app)
+    .post('/api/v1/authenticate/')
+    .send({ email: fakerPeople.email, senha: fakerPeople.senha });
 
-//   const { body } = result;
-//   BearerToken = body.token;
-// });
+  const { body } = result;
+  BearerToken = body.token;
+});
 
-// describe('API :: DELETE :: /car', () => {
-//   it('Should delete a people by Id', async () => {
-//     const fakeCar = {
-//       modelo: 'Ford Focus',
-//       cor: 'Prata',
-//       ano: 2013,
-//       acessorios: [{ descricao: 'Ar-condicionado' }, { descricao: 'Direção Hidraulica' }],
-//       quantidadePassageiros: 5
-//     };
+let fakerCar = {};
+describe('API :: DELETE :: /car', () => {
+  beforeEach(async () => {
+    fakerCar = CarDataFaker();
 
-//     const { text } = await request(app)
-//       .post('/api/v1/car/')
-//       .set('Authorization', `Bearer ${BearerToken}`)
-//       .send(fakeCar);
+    const { text } = await request(app)
+      .post('/api/v1/car/')
+      .set('Authorization', `Bearer ${BearerToken}`)
+      .send(fakerCar);
+    const { _id } = JSON.parse(text);
 
-//     const { _id } = JSON.parse(text);
+    result = await request(app).delete(`/api/v1/car/${_id.toString()}`).set('Authorization', `Bearer ${BearerToken}`);
+  });
+  it('Should delete a people by Id', (done) => {
+    const { body } = result;
 
-//     const response = await request(app)
-//       .delete(`/api/v1/car/${_id.toString()}`)
-//       .set('Authorization', `Bearer ${BearerToken}`);
-//     const { body } = response;
-//     expect(body.modelo).toBeUndefined();
-//     expect(body.cor).toBeUndefined();
-//     expect(body.ano).toBeUndefined();
-//     expect(body.acessorios).toBeUndefined();
-//     expect(body.quantidadePassageiros).toBeUndefined();
-//     const { status } = response;
-//     expect(status).toBe(204);
-//   });
-// });
+    expect(body.modelo).toBeUndefined();
+    expect(body.cor).toBeUndefined();
+    expect(body.ano).toBeUndefined();
+    expect(body.acessorios).toBeUndefined();
+    expect(body.quantidadePassageiros).toBeUndefined();
+    done();
+  });
+
+  it('Should return status code 204', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(204);
+    done();
+  });
+});
+
+describe('Should return an error id not found', () => {
+  beforeEach(async () => {
+    fakerId = '6195522596696e997f8b6806';
+
+    result = await request(app).delete(`/api/v1/car/${fakerId}`).set('Authorization', `Bearer ${BearerToken}`);
+  });
+
+  it('Should return an object with name and description properties', (done) => {
+    const { body } = result;
+
+    expect(body.name).toBe('id');
+    expect(body.description).toBe(`Id ${fakerId} not found`);
+    done();
+  });
+
+  it('should return a object with properties type string', (done) => {
+    const { body } = result;
+
+    expect(body).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status code 404', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(404);
+    done();
+  });
+});
+
+describe('Should return an error id invalid', () => {
+  beforeEach(async () => {
+    fakerId = '6195522596696e997f8b680';
+
+    result = await request(app).delete(`/api/v1/car/${fakerId}`).set('Authorization', `Bearer ${BearerToken}`);
+  });
+
+  it('Should return an object with name and description properties', (done) => {
+    const { body } = result;
+
+    expect(body[0].name).toBe('_id');
+    expect(body[0].description).toBe(
+      `"_id" with value "${fakerId}" fails to match the required pattern: /^[0-9a-fA-F]{24}$/`
+    );
+    done();
+  });
+
+  it('should return a object with properties type string', (done) => {
+    const { body } = result;
+
+    expect(body[0]).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status code 400', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(400);
+    done();
+  });
+});
