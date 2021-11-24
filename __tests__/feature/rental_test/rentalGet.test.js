4f977c50b5b23687c720193d38aa60e17da1d2e2
Luciano Holz <luciano.holz@hotmail.com>
2021-11-24 18:37:16 -0300

    Refatoring tests

--- __tests__/feature/rental_test/rentalGet.test.js
+++ __tests__/feature/rental_test/rentalGet.test.js
@@ -1,36 +1,180 @@
-// const request = require('supertest');
+const request = require('supertest');
 
-// const app = require('../../../src/app');
+const app = require('../../../src/app');
 
-// describe('API :: GET :: /rental', () => {
-//   it('Should get all rentals', async () => {
-//     const fakerRental = {
-//       nome: 'Localiza',
-//       cnpj: '78.863.735/0001-36',
-//       atividades: 'Aluga Veiculos',
-//       endereco: [
-//         {
-//           cep: '459.424.800-44',
-//           number: 123,
-//           isFilial: false
-//         }
-//       ]
-//     };
-//     await request(app).post('/api/v1/rental').send(fakerRental);
+let fakerRental = {};
+let fakerRental2 = {};
+let result = {};
+describe('API :: GET :: /rental', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza',
+      cnpj: '78.863.735/0001-36',
+      atividades: 'Aluga Veiculos',
+      endereco: [
+        {
+          cep: '96087-215',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    fakerRental2 = {
+      nome: 'Localiza Rents',
+      cnpj: '88.366.107/0001-07',
+      atividades: 'Aluguel',
+      endereco: [
+        {
+          cep: '96071-500',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    await request(app).post('/api/v1/rental').send(fakerRental);
+    await request(app).post('/api/v1/rental').send(fakerRental2);
 
-//     const { body } = await request(app).get('/api/v1/rental/');
-//     expect(body).toEqual({
-//       _id: expect.any(String),
-//       cnpj: expect.any(String),
-//       atividades: expect.any(String),
-//       endereco: expect.any(Array),
-//       limit: expect.any(Number),
-//       offset: expect.any(Number),
-//       offsets: expect.any(Number),
-//       total: expect.any(Number),
-//       locadoras: expect.any(Array)
-//     });
-//     const { status } = await request(app).get('/api/v1/rental/');
-//     expect(status).toBe(200);
-//   });
-// });
+    result = await request(app).get('/api/v1/rental/');
+  });
+
+  it('Sould get an Array locadoras with all rentals', (done) => {
+    const { body } = result;
+    const { locadoras } = body;
+
+    expect(locadoras).toHaveLength(2);
+    expect(locadoras[0].nome).toBe(fakerRental.nome);
+    expect(locadoras[0].cnpj).toBe(fakerRental.cnpj);
+    expect(locadoras[0].atividades).toBe(fakerRental.atividades);
+    expect(locadoras[0].endereco[0].cep).toBe(fakerRental.endereco[0].cep);
+    expect(locadoras[0].endereco[0].logradouro).toBeDefined();
+    expect(locadoras[0].endereco[0].complemento).toBeDefined();
+    expect(locadoras[0].endereco[0].bairro).toBeDefined();
+    expect(locadoras[0].endereco[0].number).toBe(fakerRental.endereco[0].number);
+    expect(locadoras[0].endereco[0].localidade).toBeDefined();
+    expect(locadoras[0].endereco[0].uf).toBeDefined();
+    expect(locadoras[0].endereco[0].isFilial).toBe(fakerRental.endereco[0].isFilial);
+    done();
+  });
+
+  it('Should return a body with values type string, object and number ', (done) => {
+    const { body } = result;
+    const { locadoras } = body;
+
+    expect(locadoras[0]).toEqual({
+      _id: expect.any(String),
+      nome: expect.any(String),
+      cnpj: expect.any(String),
+      atividades: expect.any(String),
+      endereco: expect.any(Object)
+    });
+    expect(locadoras[0].endereco[0]).toEqual({
+      _id: expect.any(String),
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String),
+      bairro: expect.any(String),
+      number: expect.any(Number),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      isFilial: expect.any(Boolean)
+    });
+
+    expect(locadoras[1]).toEqual({
+      _id: expect.any(String),
+      nome: expect.any(String),
+      cnpj: expect.any(String),
+      atividades: expect.any(String),
+      endereco: expect.any(Object)
+    });
+    expect(locadoras[1].endereco[0]).toEqual({
+      _id: expect.any(String),
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String),
+      bairro: expect.any(String),
+      number: expect.any(Number),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      isFilial: expect.any(Boolean)
+    });
+    done();
+  });
+
+  it('should return status code 200', async () => {
+    const { status } = result;
+
+    expect(status).toBe(200);
+  });
+});
+
+describe('Should get all rentals by their names', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza',
+      cnpj: '78.863.735/0001-36',
+      atividades: 'Aluga Veiculos',
+      endereco: [
+        {
+          cep: '96087-215',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+
+    await request(app).post('/api/v1/rental').send(fakerRental);
+
+    result = await request(app).get(`/api/v1/rental/?nome=${fakerRental.nome}`);
+  });
+
+  it('Sould get an Array locadoras with all rentals', (done) => {
+    const { body } = result;
+    const { locadoras } = body;
+
+    expect(locadoras).toHaveLength(1);
+    expect(locadoras[0]).toHaveProperty('_id');
+    expect(locadoras[0].nome).toBe(fakerRental.nome);
+    expect(locadoras[0].cnpj).toBe(fakerRental.cnpj);
+    expect(locadoras[0].atividades).toBe(fakerRental.atividades);
+    expect(locadoras[0].endereco[0].cep).toBe(fakerRental.endereco[0].cep);
+    expect(locadoras[0].endereco[0].logradouro).toBeDefined();
+    expect(locadoras[0].endereco[0].complemento).toBeDefined();
+    expect(locadoras[0].endereco[0].bairro).toBeDefined();
+    expect(locadoras[0].endereco[0].number).toBe(fakerRental.endereco[0].number);
+    expect(locadoras[0].endereco[0].localidade).toBeDefined();
+    expect(locadoras[0].endereco[0].uf).toBeDefined();
+    expect(locadoras[0].endereco[0].isFilial).toBe(fakerRental.endereco[0].isFilial);
+    done();
+  });
+
+  it('Should return a body with values type string, object and number ', (done) => {
+    const { body } = result;
+    const { locadoras } = body;
+
+    expect(locadoras[0]).toEqual({
+      _id: expect.any(String),
+      nome: expect.any(String),
+      cnpj: expect.any(String),
+      atividades: expect.any(String),
+      endereco: expect.any(Object)
+    });
+    expect(locadoras[0].endereco[0]).toEqual({
+      _id: expect.any(String),
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String),
+      bairro: expect.any(String),
+      number: expect.any(Number),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      isFilial: expect.any(Boolean)
+    });
+    done();
+  });
+
+  it('should return status code 200', async () => {
+    const { status } = result;
+
+    expect(status).toBe(200);
+  });
+});
--- __tests__/feature/rental_test/rentalPost.test.js
+++ __tests__/feature/rental_test/rentalPost.test.js
@@ -1,128 +1,249 @@
-// const request = require('supertest');
+const request = require('supertest');
+const app = require('../../../src/app');
 
-// const app = require('../../../src/app');
+let fakerRental = {};
+let result = {};
+describe('API :: POST :: /rental', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza',
+      cnpj: '70.159.778/0001-23',
+      atividades: 'Aluga Veiculos',
+      endereco: [
+        {
+          cep: '96077-686',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    result = await request(app).post('/api/v1/rental/').send(fakerRental);
+  });
 
-// describe('API :: POST :: /rental', () => {
-//   describe('', () => {
-//     let result;
-//     let fakerRental;
-//     const Prefix = '/api/v1/rental/';
-//     beforeEach(async () => {
-//       fakerRental = {
-//         nome: 'Localiza',
-//         cnpj: '70.159.778/0001-23',
-//         atividades: 'Aluga Veiculos',
-//         endereco: [
-//           {
-//             cep: '96170-000',
-//             number: '123',
-//             isFilial: false
-//           }
-//         ]
-//       };
-//       result = await request(app).post(Prefix).send(fakerRental);
-//     });
-//     it('Should return Created status 201', () => {
-//       expect(result.status).toBe(201);
-//       expect(result.body.nome).toBe(fakerRental.nome);
-//       expect(result.body.cnpj).toBe(fakerRental.cnpj);
-//       expect(result.body.atividades).toBe(fakerRental.atividades);
-//       expect(result.body.endereco[0].cep).toBe(fakerRental.endereco[0].cep);
-//       expect(result.body.endereco[0].isFilial).toBe(fakerRental.endereco[0].isFilial);
-//     });
-//   });
-//   describe('', () => {
-//     let result;
-//     const Prefix = '/api/v1/rental/';
-//     beforeEach(async () => {
-//       const fakerRental = {
-//         nome: ' ',
-//         cnpj: '78.863.735/0001-36',
-//         atividades: 'Aluga Veiculos',
-//         endereco: [
-//           {
-//             cep: '459.424.800-44',
-//             number: 123,
-//             isFilial: false
-//           }
-//         ]
-//       };
-//       result = await request(app).post(Prefix).send(fakerRental);
-//     });
-//     it('Should return status 400 with errors if nome has spaces empty', () => {
-//       expect(result.status).toBe(400);
-//       expect(result.body.length).toBeGreaterThanOrEqual(1);
-//       expect(result.body[0].name).toBe('nome');
-//       expect(result.body[0].description).toBe('"nome" is not allowed to be empty');
-//     });
-//   });
-//   describe('', () => {
-//     let result;
-//     const Prefix = '/api/v1/rental/';
-//     beforeEach(async () => {
-//       const fakerRental = {
-//         nome: 'Localiza',
-//         cnpj: '78.863.735/0001-36',
-//         atividades: ' ',
-//         endereco: [
-//           {
-//             cep: '459.424.800-44',
-//             number: 123,
-//             isFilial: false
-//           }
-//         ]
-//       };
-//       result = await request(app).post(Prefix).send(fakerRental);
-//     });
-//     it('Should return status 400 with errors if atividades has spaces empty', () => {
-//       expect(result.status).toBe(400);
-//       expect(result.body.length).toBeGreaterThanOrEqual(1);
-//       expect(result.body[0].name).toBe('atividades');
-//       expect(result.body[0].description).toBe('"atividades" is not allowed to be empty');
-//     });
-//   });
-//   describe('', () => {
-//     let result;
-//     const Prefix = '/api/v1/rental/';
-//     beforeEach(async () => {
-//       const fakerRental = {
-//         nome: 'Localiza',
-//         cnpj: '78.863.735/0001-36',
-//         atividades: ' ',
-//         endereco: [
-//           {
-//             cep: '459.424.800-44',
-//             number: 123,
-//             isFilial: false
-//           }
-//         ]
-//       };
-//       result = await request(app).post(Prefix).send(fakerRental);
-//     });
-//     it('Should return status 400 with errors if atividades has spaces empty', () => {
-//       expect(result.status).toBe(400);
-//       expect(result.body.length).toBeGreaterThanOrEqual(1);
-//       expect(result.body[0].name).toBe('atividades');
-//       expect(result.body[0].description).toBe('"atividades" is not allowed to be empty');
-//     });
-//   });
-//   describe('', () => {
-//     let result;
-//     const Prefix = '/api/v1/rental/';
-//     beforeEach(async () => {
-//       const fakerRental = {
-//         nome: 'Localiza',
-//         cnpj: '78.863.735/0001-36',
-//         atividades: 'Aluga Veiculos',
-//         endereco: []
-//       };
-//       result = await request(app).post(Prefix).send(fakerRental);
-//     });
-//     it('Should return status 400 with errors if atividades has spaces empty', () => {
-//       expect(result.status).toBe(400);
-//       expect(result.body.length).toBeGreaterThanOrEqual(1);
-//       expect(result.body[0].name).toBe('endereco');
-//       expect(result.body[0].description).toBe('"endereco" must contain at least 1 items');
-//     });
-//   });
-// });
+  it('Should return a body with _id and the same properties from fakerRental with full address', (done) => {
+    const { body } = result;
+
+    expect(body).toHaveProperty('_id');
+    expect(body.nome).toBe(fakerRental.nome);
+    expect(body.cnpj).toBe(fakerRental.cnpj);
+    expect(body.atividades).toBe(fakerRental.atividades);
+    expect(body.endereco[0].cep).toBe(fakerRental.endereco[0].cep);
+    expect(body.endereco[0].logradouro).toBeDefined();
+    expect(body.endereco[0].complemento).toBeDefined();
+    expect(body.endereco[0].bairro).toBeDefined();
+    expect(body.endereco[0].number).toBe(fakerRental.endereco[0].number);
+    expect(body.endereco[0].localidade).toBeDefined();
+    expect(body.endereco[0].uf).toBeDefined();
+    expect(body.endereco[0].isFilial).toBe(fakerRental.endereco[0].isFilial);
+    done();
+  });
+
+  it('Should return a body with values type string, object and number ', (done) => {
+    const { body } = result;
+
+    expect(body).toEqual({
+      _id: expect.any(String),
+      nome: expect.any(String),
+      cnpj: expect.any(String),
+      atividades: expect.any(String),
+      endereco: expect.any(Object)
+    });
+    expect(body.endereco[0]).toEqual({
+      _id: expect.any(String),
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String),
+      bairro: expect.any(String),
+      number: expect.any(Number),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      isFilial: expect.any(Boolean)
+    });
+    done();
+  });
+
+  it('should return status code 201', async () => {
+    const { status } = result;
+
+    expect(status).toBe(201);
+  });
+});
+
+describe('Should do not create a rental with nome field empty', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: ' ',
+      cnpj: '70.159.778/0001-23',
+      atividades: 'Aluga Veiculos',
+      endereco: [
+        {
+          cep: '96077-686',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    result = await request(app).post('/api/v1/rental/').send(fakerRental);
+  });
+
+  it('Should return name and description from error', (done) => {
+    const { body } = result;
+
+    expect(body[0].name).toBe('nome');
+    expect(body[0].description).toBe('"nome" is not allowed to be empty');
+    done();
+  });
+
+  it('Should return a body with values type string', (done) => {
+    const { body } = result;
+
+    expect(body[0]).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status 400', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(400);
+    done();
+  });
+});
+
+describe('Should do not create a rental with cnpj is invalid', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza Car',
+      cnpj: '70.159.778/0001',
+      atividades: 'Aluga Veiculos',
+      endereco: [
+        {
+          cep: '96077-686',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    result = await request(app).post('/api/v1/rental/').send(fakerRental);
+  });
+
+  it('Should return name and description from error ', (done) => {
+    const { body } = result;
+
+    expect(body[0].name).toBe('cnpj');
+    expect(body[0].description).toBe(
+      `"cnpj" with value "${fakerRental.cnpj}" fails to match the required pattern: /^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}-\\d{2}$/`
+    );
+    done();
+  });
+
+  it('Should return a body with values type string', (done) => {
+    const { body } = result;
+
+    expect(body[0]).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status 400', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(400);
+    done();
+  });
+});
+
+describe('Should do not create a rental if atividades field is empty', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza Car',
+      cnpj: '88.366.107/0001-07',
+      atividades: ' ',
+      endereco: [
+        {
+          cep: '96077-686',
+          number: 123,
+          isFilial: false
+        }
+      ]
+    };
+    result = await request(app).post('/api/v1/rental/').send(fakerRental);
+  });
+
+  it('Should return name and description from error ', (done) => {
+    const { body } = result;
+
+    expect(body[0].name).toBe('atividades');
+    expect(body[0].description).toBe(`"atividades" is not allowed to be empty`);
+    done();
+  });
+
+  it('Should return a body with values type string', (done) => {
+    const { body } = result;
+
+    expect(body[0]).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status 400', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(400);
+    done();
+  });
+});
+
+describe('Should do not create a rental if isFilial has two falses', () => {
+  beforeEach(async () => {
+    fakerRental = {
+      nome: 'Localiza Car',
+      cnpj: '88.366.107/0001-07',
+      atividades: 'Aluguel de Carros',
+      endereco: [
+        {
+          cep: '96077-686',
+          number: 123,
+          isFilial: false
+        },
+        {
+          cep: '96077-686',
+          number: 1234,
+          isFilial: false
+        }
+      ]
+    };
+    result = await request(app).post('/api/v1/rental/').send(fakerRental);
+  });
+
+  it('Should return name and description from error ', (done) => {
+    const { body } = result;
+
+    expect(body.name).toBe('Conflict');
+    expect(body.description).toBe(`IsFilial false is possible only in one address`);
+    done();
+  });
+
+  it('Should return a body with values type string', (done) => {
+    const { body } = result;
+
+    expect(body).toEqual({
+      name: expect.any(String),
+      description: expect.any(String)
+    });
+    done();
+  });
+
+  it('Should return status 400', (done) => {
+    const { status } = result;
+
+    expect(status).toBe(400);
+    done();
+  });
+});
