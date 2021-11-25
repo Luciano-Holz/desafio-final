afb1618f3a5df0ae1de9f94020c629a18c11c0a2
Luciano Holz <luciano.holz@hotmail.com>
2021-11-25 16:19:59 -0300

    Changes on method create

--- src/app/service/FleetService.js
+++ src/app/service/FleetService.js
@@ -1,14 +1,29 @@
 const BadRequest = require('../errors/BadRequest');
 const NotFound = require('../errors/NotFound');
 const FleetRepository = require('../repository/FleetRepository');
+const RentalRepository = require('../repository/RentalRepository');
+const CarRepository = require('../repository/CarRepository');
 
 class FleetService {
   async create(_id, payload) {
-    const checkPlaca = await FleetRepository.getAll({ placa: payload.placa });
+    const { id_locadora, id_carro, placa } = payload;
+    if (id_locadora !== _id) throw new BadRequest('Id Rental', 'Invalid');
+
+    const rental = await RentalRepository.getById(id_locadora);
+    if (!rental) throw new NotFound('rental', 'not found');
+
+    const car = await CarRepository.getById(id_carro);
+    if (!car) throw new NotFound('car', 'not found');
+
+    const checkPlaca = await FleetRepository.getAll({ placa });
     if (checkPlaca.docs.length > 0) {
-      throw new BadRequest('Conflict', `This placa ${payload.placa} already exists in this Rental`);
+      throw new BadRequest('Conflict', `This placa ${placa} already exists in this Rental`);
     }
-    const result = await FleetRepository.create(_id, payload);
+
+    const result = await FleetRepository.create(payload);
+    if (!result) {
+      throw new NotFound('Fleet', `not found`);
+    }
     return result;
   }
 
@@ -18,7 +33,7 @@ class FleetService {
   }
 
   async getById(_id, _idFleet) {
-    const result = await FleetRepository.getById(_id, _idFleet);
+    const result = await FleetRepository.getById(_idFleet);
     return result;
   }
 
