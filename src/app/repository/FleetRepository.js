20770ff21bc4b07bc09dc820e25de4e05321a28b
Luciano Holz <luciano.holz@hotmail.com>
2021-11-25 12:12:31 -0300

    Add constructor and extends Repository

--- src/app/repository/FleetRepository.js
+++ src/app/repository/FleetRepository.js
@@ -1,37 +1,9 @@
 const FleetSchema = require('../schema/FleetSchema');
+const Repository = require('./Repository');
 
-class FleetRepository {
-  async create(_id, payload) {
-    const result = await FleetSchema.create(_id, payload);
-    return result;
-  }
-
-  async getAll(queryParams) {
-    const { page = 1, limit = 100, ...query } = queryParams;
-    const result = await FleetSchema.paginate(
-      { ...query },
-      {
-        limit: Number(limit),
-        page: Number(page),
-        skip: (Number(page) - 1) * Number(limit)
-      }
-    );
-    return result;
-  }
-
-  async getById(_id) {
-    const result = await FleetSchema.findById({ _id });
-    return result;
-  }
-
-  async update(_id, payload) {
-    const result = await FleetSchema.findByIdAndUpdate({ _id }, payload, { new: true });
-    return result;
-  }
-
-  async delete(_id) {
-    const result = await FleetSchema.findByIdAndDelete({ _id });
-    return result;
+class FleetRepository extends Repository {
+  constructor() {
+    super(FleetSchema);
   }
 }
 
