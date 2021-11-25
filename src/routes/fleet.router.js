4fe5ae459784ee710bcb03d67ba6d330c5dcf3b4
Luciano Holz <luciano.holz@hotmail.com>
2021-11-25 19:51:49 -0300

    Add idFleetValidation on getId, put and delete methods

--- src/routes/fleet.router.js
+++ src/routes/fleet.router.js
@@ -1,12 +1,13 @@
 const FleetController = require('../app/controller/FleetController');
 const createValidation = require('../app/validation/fleet/create');
+const idFleetValidation = require('../app/validation/fleet/idFleetValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/rental/') => {
   routes.post('/:_id/fleet', createValidation, FleetController.create);
   routes.get('/:_id/fleet', FleetController.getAll);
-  routes.get('/:_id/fleet/:_idFleet', FleetController.getById);
-  routes.put('/:_id/fleet/:_idFleet', createValidation, FleetController.update);
-  routes.delete('/:_id/fleet/:_idFleet', FleetController.delete);
+  routes.get('/:_id/fleet/:_idFleet', idFleetValidation, FleetController.getById);
+  routes.put('/:_id/fleet/:_idFleet', idFleetValidation, createValidation, FleetController.update);
+  routes.delete('/:_id/fleet/:_idFleet', idFleetValidation, FleetController.delete);
 
   server.use(prefix, routes);
 };
