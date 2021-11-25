82b1050dd64b7ce6045ecd8b6516827013fc68ef
Luciano Holz <luciano.holz@hotmail.com>
2021-11-25 10:34:50 -0300

    Refatoring code on routes path

--- src/routes/fleet.router.js
+++ src/routes/fleet.router.js
@@ -1,11 +1,12 @@
 const FleetController = require('../app/controller/FleetController');
+const createValidation = require('../app/validation/fleet/create');
 
-module.exports = (server, routes, prefix = '/api/v1/rental/:_id/fleet') => {
-  routes.post('/', FleetController.create);
-  routes.get('/', FleetController.getAll);
-  routes.get('/:_id', FleetController.getById);
-  routes.put('/:_id', FleetController.update);
-  routes.delete('/:_id', FleetController.delete);
+module.exports = (server, routes, prefix = '/api/v1/rental/') => {
+  routes.post('/:_id/fleet', createValidation, FleetController.create);
+  routes.get('/:_id/fleet', FleetController.getAll);
+  routes.get('/:_id/fleet/:_idFleet', FleetController.getById);
+  routes.put('/:_id/fleet/:_idFleet', createValidation, FleetController.update);
+  routes.delete('/:_id/fleet/:_idFleet', FleetController.delete);
 
   server.use(prefix, routes);
 };
