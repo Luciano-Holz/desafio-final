5fa9dd5fe978f3fb50f0504fc113d5b6b0c265a5
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 11:18:41 -0300

    chore: changes in create method

--- src/app/service/ReserveService.js
+++ src/app/service/ReserveService.js
@@ -1,8 +1,32 @@
+const moment = require('moment');
 const ReserveRepository = require('../repository/ReserveRepository');
+const FleetRepository = require('../repository/FleetRepository');
+const NotFound = require('../errors/NotFound');
+const BadRequest = require('../errors/BadRequest');
+const PeopleService = require('./PeopleService');
+const RentalService = require('./RentalService');
 
 class ReserveService {
   async create(_id, payload) {
-    const result = await ReserveRepository.create(_id, payload);
+    const { data_inicio, data_fim, id_user, id_carro, id_locadora } = payload;
+    if (id_locadora !== _id) throw new BadRequest('Id Rental', 'Invalid');
+
+    const people = await PeopleService.getById(id_user);
+    if (!people) throw new NotFound('people', 'not found in database');
+
+    const dateStart = moment(data_inicio, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    const dateEnd = moment(data_fim, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    payload.data_inicio = dateStart;
+    payload.data_fim = dateEnd;
+
+    const carFleet = await FleetRepository.getById(id_carro);
+    if (!carFleet) throw new NotFound('car', 'not found in fleet');
+
+    const rental = await RentalService.getById(id_locadora);
+    if (!rental) throw new NotFound('rental', 'not found in database');
+
+    const result = await ReserveRepository.create(payload);
+    if (!result) throw new NotFound('Reserve', 'not found');
     return result;
   }
 
