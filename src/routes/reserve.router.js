b60649efc68f93c1d59c60823224d9ad4ef533cd
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 15:13:13 -0300

    chore: add idReserveValidation at getId, put and delete routes

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,12 +1,13 @@
 const ReserveController = require('../app/controller/ReserveController');
 const createValidation = require('../app/validation/reserve/create');
+const idReserveValidation = require('../app/validation/reserve/idReserveValidation');
 
 module.exports = (server, routes, prefix = '/api/v1/rental/') => {
   routes.post('/:_id/reserve', createValidation, ReserveController.create);
   routes.get('/:_id/reserve', ReserveController.getAll);
-  routes.get('/:_id/reserve/:_idReserve', ReserveController.getById);
-  routes.put('/:_id/reserve/:_idReserve', createValidation, ReserveController.update);
-  routes.delete('/:_id/reserve/:_idReserve', ReserveController.delete);
+  routes.get('/:_id/reserve/:_idReserve', idReserveValidation, ReserveController.getById);
+  routes.put('/:_id/reserve/:_idReserve', idReserveValidation, createValidation, ReserveController.update);
+  routes.delete('/:_id/reserve/:_idReserve', idReserveValidation, ReserveController.delete);
 
   server.use(prefix, routes);
 };
