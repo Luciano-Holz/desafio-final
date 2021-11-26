31dc98bb0fc176286c9038cebb73014e2c0575fd
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 09:04:28 -0300

    chore: create files create.js and idReserveValidation.js with validations from reserve and idReserve

---
+++ src/app/validation/reserve/create.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      id_user: Joi.string()
+        .pattern(/^[0-9a-fA-F]{24}$/)
+        .required(),
+      data_inicio: Joi.date().format('DD/MM/YYYY').required(),
+      data_fim: Joi.date().format('DD/MM/YYYY').required(),
+      id_carro: Joi.string()
+        .pattern(/^[0-9a-fA-F]{24}$/)
+        .required(),
+      id_locadora: Joi.string()
+        .pattern(/^[0-9a-fA-F]{24}$/)
+        .required(),
+      valor_final: Joi.number().required()
+    });
+
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(
+      error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    );
+  }
+};
---
+++ src/app/validation/reserve/idReserveValidation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      _id: Joi.string()
+        .pattern(/^[0-9a-fA-F]{24}$/)
+        .required(),
+      _idReserve: Joi.string()
+        .pattern(/^[0-9a-fA-F]{24}$/)
+        .required()
+    });
+
+    const { error } = await schema.validate(req.params, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(
+      error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    );
+  }
+};
