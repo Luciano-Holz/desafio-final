1d7ff7ba0fb9bac566a105ad8ba0e552a4cbdbc1
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 11:05:59 -0300

    chore: add constructor and extends Repository

--- src/app/repository/ReserveRepository.js
+++ src/app/repository/ReserveRepository.js
@@ -1,36 +1,13 @@
 const ReserveSchema = require('../schema/ReserveSchema');
+const Repository = require('./Repository');
 
-class ReserveRepository {
-  async create(_id, payload) {
-    const result = await ReserveSchema.create(payload);
-    return result;
+class ReserveRepository extends Repository {
+  constructor() {
+    super(ReserveSchema);
   }
 
-  async getAll(queryParams) {
-    const { page = 1, limit = 100, ...query } = queryParams;
-    const result = await ReserveSchema.paginate(
-      { ...query },
-      {
-        limit: Number(limit),
-        page: Number(page),
-        skip: (Number(page) - 1) * Number(limit)
-      }
-    );
-    return result;
-  }
-
-  async getById(_id, _idReserve) {
-    const result = await ReserveSchema.findById({ _id: _idReserve });
-    return result;
-  }
-
-  async update(_id, payload) {
-    const result = await ReserveSchema.findByIdAndUpdate({ _id }, payload, { new: true });
-    return result;
-  }
-
-  async delete(_id) {
-    const result = await ReserveSchema.findByIdAndDelete({ _id });
+  async update(_id, _idReserve, payload) {
+    const result = await this.schema.findByIdAndUpdate({ id_locadora: _id, _id: _idReserve }, payload, { new: true });
     return result;
   }
 }
