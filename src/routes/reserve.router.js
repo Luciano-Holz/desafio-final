bb39fa573b0a7fe2c0ff9793ecb1c97ea3620760
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 11:05:02 -0300

    chore: add createValidation in post and put routes

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,10 +1,11 @@
 const ReserveController = require('../app/controller/ReserveController');
+const createValidation = require('../app/validation/reserve/create');
 
 module.exports = (server, routes, prefix = '/api/v1/rental/') => {
-  routes.post('/:_id/reserve', ReserveController.create);
+  routes.post('/:_id/reserve', createValidation, ReserveController.create);
   routes.get('/:_id/reserve', ReserveController.getAll);
   routes.get('/:_id/reserve/:_idReserve', ReserveController.getById);
-  routes.put('/:_id/reserve/:_idReserve', ReserveController.update);
+  routes.put('/:_id/reserve/:_idReserve', createValidation, ReserveController.update);
   routes.delete('/:_id/reserve/:_idReserve', ReserveController.delete);
 
   server.use(prefix, routes);
