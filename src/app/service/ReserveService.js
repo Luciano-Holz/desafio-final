ca394a9ed72964d5fed77df577da07ca1e12035d
Luciano Holz <luciano.holz@hotmail.com>
2021-11-26 21:51:45 -0300

    chore: add validations in create method

--- src/app/service/ReserveService.js
+++ src/app/service/ReserveService.js
@@ -3,7 +3,7 @@ const ReserveRepository = require('../repository/ReserveRepository');
 const FleetRepository = require('../repository/FleetRepository');
 const NotFound = require('../errors/NotFound');
 const BadRequest = require('../errors/BadRequest');
-const PeopleService = require('./PeopleService');
+const PeopleService = require('./People');
 const RentalService = require('./RentalService');
 
 class ReserveService {
@@ -11,16 +11,32 @@ class ReserveService {
     const { data_inicio, data_fim, id_user, id_carro, id_locadora } = payload;
     if (id_locadora !== _id) throw new BadRequest('Id Rental', 'Invalid');
 
-    const people = await PeopleService.getById(id_user);
-    if (!people) throw new NotFound('people', 'not found in database');
+    const user = await PeopleService.getById(id_user);
+    if (!user) throw new NotFound('people', 'not found in database');
 
-    const dateStart = moment(data_inicio, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    const dateEnd = moment(data_fim, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    payload.data_inicio = dateStart;
-    payload.data_fim = dateEnd;
+    const { habilitado } = user;
+    if (habilitado !== 'sim') throw new BadRequest('hablitado', 'Não está habilitado');
 
     const carFleet = await FleetRepository.getById(id_carro);
     if (!carFleet) throw new NotFound('car', 'not found in fleet');
+
+    const now = moment().format('YYYY-MM-DD');
+    const dateStart = moment(data_inicio, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    const dateEnd = moment(data_fim, 'DD/MM/YYYY').format('YYYY-MM-DD');
+
+    const dateStartValid = moment(dateStart).isSameOrAfter(now);
+    if (!dateStartValid) throw new BadRequest('data_inicio', 'Can not be earlier than today');
+
+    const dateEndValid = moment(dateEnd).isSameOrAfter(dateStart);
+    if (!dateEndValid) throw new BadRequest('data_fim', 'Can not be earlier than data_inicio');
+
+    const { valor_diaria } = carFleet;
+    const daysRent = moment(dateEnd).diff(dateStart, 'days');
+    const finalValue = valor_diaria * daysRent;
+
+    payload.data_inicio = dateStart;
+    payload.data_fim = dateEnd;
+    payload.valor_final = finalValue;
 
     const rental = await RentalService.getById(id_locadora);
     if (!rental) throw new NotFound('rental', 'not found in database');
